3108b2f605158f21ed586c73959b0cb26bfbbc5e
Hyunkuk <boris0815@naver.com>
2023-05-16 18:07:32 +0900

    [refactor] : rename route to graph

--- presenter/src/main/java/com/mashup/twotoo/presenter/navigation/TwoTooNavHost.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/navigation/TwoTooNavHost.kt
@@ -3,12 +3,12 @@ package com.mashup.twotoo.presenter.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
-import com.mashup.twotoo.presenter.garden.navigation.gardenRoute
+import com.mashup.twotoo.presenter.garden.navigation.gardenGraph
 import com.mashup.twotoo.presenter.home.navigation.homeNavigationRoute
-import com.mashup.twotoo.presenter.home.navigation.homeRoute
-import com.mashup.twotoo.presenter.onboarding.navigation.onBoardingRoute
+import com.mashup.twotoo.presenter.home.navigation.homeGraph
+import com.mashup.twotoo.presenter.onboarding.navigation.onBoardingGraph
 import com.mashup.twotoo.presenter.ui.TwoTooAppState
-import com.mashup.twotoo.presenter.user.navigation.userRoute
+import com.mashup.twotoo.presenter.user.navigation.userGraph
 
 @Composable
 fun TwoTooNavHost(
@@ -22,9 +22,9 @@ fun TwoTooNavHost(
         startDestination = startDestination,
         modifier = modifier,
     ) {
-        gardenRoute()
-        homeRoute()
-        userRoute()
-        onBoardingRoute()
+        gardenGraph()
+        homeGraph()
+        userGraph()
+        onBoardingGraph()
     }
 }
