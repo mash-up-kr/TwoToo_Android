c349162bb6110644be6010825627a12afa43be2b
Hyunkuk <boris0815@naver.com>
2023-05-16 17:39:09 +0900

    [feat] : create bottom navigation and navigation item

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/Navigation.kt
@@ -0,0 +1,68 @@
+package com.mashup.twotoo.presenter.designsystem.component
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.mashup.twotoo.presenter.designsystem.theme.SelectedIconColor
+import com.mashup.twotoo.presenter.designsystem.theme.UnSelectedIconColor
+
+/**
+ * @Created by 김현국 2023/05/16
+ * @Time 4:56 PM
+ */
+
+@Composable
+fun TwoTooNavigationBar(
+    modifier: Modifier = Modifier,
+    containerColor: Color,
+    content: @Composable RowScope.() -> Unit,
+) {
+    NavigationBar(
+        modifier = modifier,
+        containerColor = containerColor,
+        contentColor = TwoTooNavigationDefaults.navigationContentColor(),
+        content = content,
+    )
+}
+
+@Composable
+fun RowScope.TwoTooNavigationBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    selectedIcon: @Composable () -> Unit = icon,
+    enabled: Boolean = true,
+    label: @Composable (() -> Unit)? = null,
+    alwaysShowLabel: Boolean = false,
+) {
+    NavigationBarItem(
+        selected = selected,
+        onClick = onClick,
+        icon = if (selected) selectedIcon else icon,
+        modifier = modifier,
+        enabled = enabled,
+        label = label,
+        alwaysShowLabel = alwaysShowLabel,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = TwoTooNavigationDefaults.navigationSelectedItemColor(),
+            unselectedIconColor = TwoTooNavigationDefaults.navigationContentColor(),
+            indicatorColor = TwoTooNavigationDefaults.navigationIndicatorColor(),
+        ),
+    )
+}
+
+object TwoTooNavigationDefaults {
+    @Composable
+    fun navigationContentColor() = UnSelectedIconColor
+
+    @Composable
+    fun navigationSelectedItemColor() = SelectedIconColor
+
+    @Composable
+    fun navigationIndicatorColor() = Color.White
+}
