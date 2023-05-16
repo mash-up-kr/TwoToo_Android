89918c3bfe28d27d7f9b5b69a4576f1277cb2ca2
Hyunkuk <boris0815@naver.com>
2023-05-16 17:40:09 +0900

    [feat] : create Icon design system

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/icon/TwoTooIcon.kt
@@ -0,0 +1,22 @@
+package com.mashup.twotoo.presenter.designsystem.icon
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.mashup.twotoo.presenter.R
+
+/**
+ * @Created by 김현국 2023/05/16
+ * @Time 3:17 PM
+ */
+object TwoTooIcon {
+    val GardenSelected = R.drawable.garden_selected
+    val GardenUnSelected = R.drawable.garden_unselected
+    val HomeSelected = R.drawable.home_selected
+    val HomeUnSelected = R.drawable.home_unselected
+    val UserSelected = R.drawable.user_selected
+    val UserUnSelected = R.drawable.user_unselected
+}
+sealed class Icon {
+    data class ImageVectorIcon(val imageVector: ImageVector) : Icon()
+    data class DrawableResourceIcon(@DrawableRes val id: Int) : Icon()
+}
