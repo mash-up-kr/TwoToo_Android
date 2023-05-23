3fbd90272796a074d857b0418c3be15a8da6fa6e
Hyunkuk <boris0815@naver.com>
2023-05-23 21:03:58 +0900

    [feat] : create Image component

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/ImageView.kt
@@ -0,0 +1,210 @@
+package com.mashup.twotoo.presenter.designsystem.component
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.R
+import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.glide.GlideImageState
+
+@Composable
+fun TwoTooImageView(
+    model: Any?,
+    @DrawableRes previewPlaceholder: Int?,
+    loadingPlaceHolder: @Composable (BoxScope.(GlideImageState.Loading) -> Unit)?,
+    failurePlaceHolder: @Composable (BoxScope.(GlideImageState.Failure) -> Unit)?,
+    modifier: Modifier = Modifier,
+) {
+    TwoTooImageViewImpl(
+        model = model,
+        enableSetImage = false,
+        onClickSetImage = null,
+        previewPlaceholder = previewPlaceholder,
+        loadingPlaceHolder = loadingPlaceHolder,
+        failurePlaceHolder = failurePlaceHolder,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun TwoTooImageViewWithSetter(
+    @DrawableRes previewPlaceholder: Int?,
+    loadingPlaceHolder: @Composable (BoxScope.(GlideImageState.Loading) -> Unit)?,
+    failurePlaceHolder: @Composable (BoxScope.(GlideImageState.Failure) -> Unit)?,
+    modifier: Modifier = Modifier,
+) {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent(),
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    TwoTooImageViewImpl(
+        model = if (imageUri != null) {
+            imageUri
+        } else {
+            R.drawable.empty_image_color_placeholder
+        },
+
+        enableSetImage = true,
+        onClickSetImage = {
+            launcher.launch("image/*")
+        },
+        previewPlaceholder = previewPlaceholder,
+        loadingPlaceHolder = loadingPlaceHolder,
+        failurePlaceHolder = failurePlaceHolder,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun TwoTooImageViewImpl(
+    model: Any?,
+    enableSetImage: Boolean,
+    onClickSetImage: (() -> Unit)?,
+    @DrawableRes previewPlaceholder: Int?,
+    loadingPlaceHolder: @Composable (BoxScope.(GlideImageState.Loading) -> Unit)?,
+    failurePlaceHolder: @Composable (BoxScope.(GlideImageState.Failure) -> Unit)?,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+    ) {
+        GlideImage(
+            modifier = Modifier.fillMaxSize(),
+            imageModel = { model ?: R.drawable.empty_image_color_placeholder },
+            previewPlaceholder = previewPlaceholder ?: R.drawable.empty_image_color_placeholder,
+            imageOptions = ImageOptions(
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center,
+            ),
+            loading = loadingPlaceHolder,
+            failure = failurePlaceHolder,
+        )
+        if (enableSetImage) {
+            PlusLine(
+                modifier = Modifier
+                    .fillMaxSize(0.5f)
+                    .align(Alignment.Center).clickable {
+                        if (onClickSetImage != null) {
+                            onClickSetImage()
+                        }
+                    },
+
+            )
+        }
+    }
+}
+
+@Composable
+fun PlusLine(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            // Fetching width and height for
+            // setting start x and end y
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            // drawing a line between start(x,y) and end(x,y)
+            drawLine(
+                start = Offset(x = (canvasWidth / 2) - (canvasWidth / 7), y = (canvasHeight / 2)),
+                end = Offset(x = (canvasWidth / 2) + (canvasWidth / 7), y = (canvasHeight / 2)),
+                color = Color.White,
+                cap = StrokeCap.Round,
+                strokeWidth = 4.dp.toPx(),
+            )
+            drawLine(
+                start = Offset(x = (canvasWidth / 2), y = (canvasHeight / 2) - (canvasHeight / 7)),
+                end = Offset(x = (canvasWidth / 2), y = (canvasHeight / 2) + (canvasHeight / 7)),
+                color = Color.White,
+                cap = StrokeCap.Round,
+                strokeWidth = 4.dp.toPx(),
+            )
+        }
+    }
+}
+
+@Preview(
+    name = "이미지가 없을 때",
+    showBackground = true,
+)
+@Composable
+fun HasNoneImageView() {
+    TwoTooTheme {
+        TwoTooImageViewWithSetter(
+            modifier = Modifier
+                .size(250.dp)
+                .clip(
+                    RoundedCornerShape(10.dp),
+                )
+                .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(10.dp)),
+            previewPlaceholder = R.drawable.empty_image_color_placeholder,
+            loadingPlaceHolder = {
+            },
+            failurePlaceHolder = {},
+        )
+    }
+}
+
+@Preview(
+    name = "이미지가 있을 때",
+    showBackground = true,
+)
+@Composable
+fun HasImageView() {
+    TwoTooTheme {
+        TwoTooImageView(
+            modifier = Modifier
+                .size(250.dp)
+                .clip(
+                    RoundedCornerShape(10.dp),
+                )
+                .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(10.dp)),
+            model = null,
+            previewPlaceholder = R.drawable.empty_image_placeholder,
+            loadingPlaceHolder = {
+            },
+            failurePlaceHolder = {},
+        )
+    }
+}
+
+@Preview("플러스 버튼")
+@Composable
+fun PlusButton() {
+    TwoTooTheme {
+        PlusLine(
+            modifier = Modifier.size(200.dp),
+        )
+    }
+}
