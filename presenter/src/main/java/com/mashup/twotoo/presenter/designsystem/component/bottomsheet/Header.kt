1adf859a70cf5a09ea385e3cefa740b320d953d3
Hyunkuk <boris0815@naver.com>
2023-05-24 15:11:01 +0900

    [fix] : add fontweight

--- presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/bottomsheet/Header.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/bottomsheet/Header.kt
@@ -3,6 +3,7 @@ package com.mashup.twotoo.presenter.designsystem.component.bottomsheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
@@ -14,5 +15,6 @@ fun Header(
         modifier = modifier,
         text = titleText,
         textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold,
     )
 }
