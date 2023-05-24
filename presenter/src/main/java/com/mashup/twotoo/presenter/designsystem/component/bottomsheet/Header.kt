94a76ba9f1f9079a736f9e3ca8818f3575eda3f6
Hyunkuk <boris0815@naver.com>
2023-05-24 14:35:16 +0900

    [feat] : Header Composable

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/bottomsheet/Header.kt
@@ -0,0 +1,18 @@
+package com.mashup.twotoo.presenter.designsystem.component.bottomsheet
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun Header(
+    titleText: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        modifier = modifier,
+        text = titleText,
+        textAlign = TextAlign.Center,
+    )
+}
