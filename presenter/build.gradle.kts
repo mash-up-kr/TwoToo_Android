326c525819b2488bf94b4dda25777d84aab6db6f
hj1115hj <hyejin_one@naver.com>
2023-05-26 01:19:12 +0900

    refactor: 린트 변경 - 리포멧과 함께 되었어요ㅜ

--- presenter/build.gradle.kts
+++ presenter/build.gradle.kts
@@ -31,8 +31,7 @@ android {
         targetCompatibility(JavaVersion.VERSION_1_8)
     }
     kotlinOptions {
-
-    jvmTarget = "1.8"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
