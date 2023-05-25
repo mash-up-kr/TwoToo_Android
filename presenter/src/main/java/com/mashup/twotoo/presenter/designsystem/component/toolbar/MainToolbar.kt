a128d06082c3e4ca46a14d707e02d758727f8ef5
hj1115hj <hyejin_one@naver.com>
2023-05-26 00:59:44 +0900

    refactor: app text string resource로 변경 -리포멧과 함께 변경되었어요

--- presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/toolbar/MainToolbar.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/toolbar/MainToolbar.kt
@@ -1,12 +1,12 @@
 package com.mashup.twotoo.presenter.designsystem.component.toolbar
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,23 +14,22 @@ import androidx.compose.ui.unit.sp
 import com.mashup.twotoo.presenter.R
 import com.mashup.twotoo.presenter.designsystem.theme.*
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainToolbar(
     onClickBackIcon: () -> Unit,
     onClickCalendarIcon: () -> Unit,
-    onClickAlarmIcon: () -> Unit
+    onClickAlarmIcon: () -> Unit,
 ) {
     TopAppBar(
         title = {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterStart) {
                 Text(
-                    text = "Twotoo",
+                    text = stringResource(id = R.string.app_name),
                     color = FontBlack,
                     fontFamily = Font.Montserrat,
                     fontSize = 26.sp,
-                    textAlign = TextAlign.Center
+                    textAlign = TextAlign.Center,
                 )
             }
         },
@@ -41,25 +40,26 @@ fun MainToolbar(
                 }) {
                     Icon(
                         painter = painterResource(id = R.drawable.back_arrow),
-                        contentDescription = null
+                        contentDescription = null,
                     )
                 }
             }
         },
         actions = {
             Row(
-                Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically
+                Modifier.fillMaxHeight(),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 IconButton(onClick = { onClickCalendarIcon() }) {
                     Icon(
                         painter = painterResource(id = R.drawable.calendar),
-                        contentDescription = null
+                        contentDescription = null,
                     )
                 }
                 IconButton(onClick = { onClickAlarmIcon() }) {
                     Icon(
                         painter = painterResource(id = R.drawable.alarm_tmp),
-                        contentDescription = null
+                        contentDescription = null,
                     )
                 }
             }
@@ -67,7 +67,7 @@ fun MainToolbar(
         modifier = Modifier.height(56.dp),
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = BackGroundWhite,
-        )
+        ),
     )
 }
 
@@ -78,32 +78,30 @@ fun MainToolbar() {
         title = {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterStart) {
                 Text(
-                    text = "Twotoo",
+                    text = stringResource(id = R.string.app_name),
                     color = FontBlack,
                     fontFamily = Font.Montserrat,
                     fontSize = 26.sp,
-                    textAlign = TextAlign.Center
+                    textAlign = TextAlign.Center,
                 )
             }
         },
         modifier = Modifier.height(56.dp),
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = BackGroundWhite,
-            titleContentColor = Purple40
-        )
+            titleContentColor = Purple40,
+        ),
     )
-
 }
-
 
 @Composable
 @Preview
 fun mainToolbarWithNavAndActionsPreview() {
-    MainToolbar({},{},{})
+    MainToolbar({}, {}, {})
 }
 
 @Composable
 @Preview
-fun mainToolbar(){
+fun mainToolbar() {
     MainToolbar()
 }
