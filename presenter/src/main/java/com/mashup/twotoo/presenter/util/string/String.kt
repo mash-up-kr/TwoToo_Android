eeec29f3e927d678b6a0a8e93c85b4383b55c635
Hyunkuk <boris0815@naver.com>
2023-06-02 13:18:23 +0900

    [refactor] : remove string util

--- presenter/src/main/java/com/mashup/twotoo/presenter/util/string/String.kt
+++
@@ -1,8 +0,0 @@
-package com.mashup.twotoo.presenter.util.string
-
-import android.content.Context
-import androidx.annotation.StringRes
-
-fun getString(context: Context, @StringRes id: Int): String {
-    return context.getString(id)
-}
