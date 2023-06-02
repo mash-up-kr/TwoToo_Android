bfd242714d4694e08bf992c5d36a4c4f3fc5ee94
Hyunkuk <boris0815@naver.com>
2023-06-02 21:51:10 +0900

    [refactor] : image state hoisting

--- presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/bottomsheet/Authenticate.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/bottomsheet/Authenticate.kt
@@ -1,10 +1,12 @@
 package com.mashup.twotoo.presenter.designsystem.component.bottomsheet
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
@@ -14,6 +16,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ExperimentalMotionApi
 import androidx.constraintlayout.compose.MotionLayout
@@ -21,15 +24,19 @@ import androidx.constraintlayout.compose.MotionScene
 import androidx.constraintlayout.compose.layoutId
 import com.mashup.twotoo.presenter.R
 import com.mashup.twotoo.presenter.designsystem.component.TwoTooImageViewWithSetter
+import com.mashup.twotoo.presenter.designsystem.component.bottomsheet.BottomSheetType.Authenticate
+import com.mashup.twotoo.presenter.designsystem.component.textfield.TwoTooTextField
 import com.mashup.twotoo.presenter.util.addFocusCleaner
 import com.mashup.twotoo.presenter.util.keyboardAsState
 
 @OptIn(ExperimentalMotionApi::class)
 @Composable
 fun AuthenticateContent(
-    titleText: String,
-    button: @Composable (Modifier) -> Unit,
+    type: Authenticate,
+    button: @Composable (Modifier, BottomSheetData) -> Unit,
 ) {
+    val titleText = stringResource(id = type.title)
+    val textHint = stringResource(id = type.textHint)
     val context = LocalContext.current
     val motionScene = remember {
         context.resources.openRawResource(R.raw.bottom_sheet)
@@ -53,6 +60,17 @@ fun AuthenticateContent(
         }
     }
 
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent(),
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
     MotionLayout(
         motionScene = MotionScene(content = motionScene),
         progress = progress,
@@ -73,6 +91,10 @@ fun AuthenticateContent(
                 .clip(
                     RoundedCornerShape(10.dp),
                 ),
+            imageUri = imageUri,
+            onClickSetImage = {
+                launcher.launch("image/*")
+            },
             previewPlaceholder = R.drawable.empty_image_color_placeholder,
             failurePlaceHolder = {},
             loadingPlaceHolder = {},
@@ -81,17 +103,22 @@ fun AuthenticateContent(
         var textFieldState by rememberSaveable {
             mutableStateOf("")
         }
-        TextField(
+        TwoTooTextField(
             modifier = Modifier.layoutId("textField").focusRequester(
                 focusRequester = focusRequester,
             ).onFocusChanged {
                 animateSwitch = it.isFocused
             },
-            value = textFieldState,
-            onValueChange = { textFieldState = it },
+            text = textFieldState,
+            textHint = textHint,
+            updateText = { textFieldState = it },
         )
         button(
             Modifier.layoutId("button"),
+            BottomSheetData.Authenticate(
+                image = imageUri,
+                text = textFieldState,
+            ),
         )
     }
 }
