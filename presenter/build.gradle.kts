0a306efac68f3142496ce3c0f8a9929f5fd91e15
ddyeon <cab05102@naver.com>
2023-06-04 16:12:02 +0900

    [chore] add pager indicator dependency

--- presenter/build.gradle.kts
+++ presenter/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.navigation)
     implementation(libs.kakao.login)
     implementation(libs.landscapist.glide)
-    implementation(libs.accompanist.pager)
+    implementation(libs.bundles.pager)
     kapt(libs.google.dagger.compiler)
     implementation(libs.bundles.orbit)
     testImplementation(libs.test.junit)
