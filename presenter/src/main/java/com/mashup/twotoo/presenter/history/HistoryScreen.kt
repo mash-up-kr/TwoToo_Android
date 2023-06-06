fb36368ffca1d30e159f3ca9a4fd9e9c7c8c7a72
hj1115hj <hyejin_one@naver.com>
2023-06-07 01:01:14 +0900

    feat: historyScreen 구현

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/history/HistoryScreen.kt
@@ -0,0 +1,82 @@
+package com.mashup.twotoo.presenter.history
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.designsystem.component.toolbar.TwoTooBackToolbar
+import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
+import com.mashup.twotoo.presenter.history.model.HistoryInfoUiModel
+import com.mashup.twotoo.presenter.history.model.HistoryItemUiModel
+
+@Composable
+fun HistoryScreen() {
+    Scaffold(
+        topBar = {
+            TwoTooBackToolbar(
+                modifier = Modifier.height(56.dp),
+                onClickBackIcon = { },
+            )
+        },
+        containerColor = TwoTooTheme.color.backgroundYellow,
+    ) {
+        val historyItemUiModels: List<HistoryItemUiModel> = generateDummyHistoryItemsToPreView()
+        Column(modifier = Modifier.fillMaxSize().padding(paddingValues = it)) {
+            ChallengeInfo(
+                "30분 이상 운동하기",
+                "운동 사진으로 인증하기\n 인증 실패하는지 확인",
+            )
+            Divider(
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth().width(1.dp).padding(horizontal = 24.dp),
+            )
+            Box {
+                DottedLine()
+                HistoryItems(historyItemUiModels)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHistoryScreen() {
+    TwoTooTheme {
+        HistoryScreen()
+    }
+}
+
+fun generateDummyHistoryItemsToPreView(): List<HistoryItemUiModel> {
+    return listOf(
+        HistoryItemUiModel(
+            partnerInfo = HistoryInfoUiModel(
+                "https://shop.biumfood.com/upload/1623296512image_product044.jpg",
+                "공주",
+                "20:35",
+            ),
+            myInfo = HistoryInfoUiModel(
+                "https://shop.biumfood.com/upload/1623296512image_product044.jpg",
+                "나",
+                "20:35",
+            ),
+            createDate = "4/10",
+        ),
+        HistoryItemUiModel(
+            partnerInfo = HistoryInfoUiModel(
+                "https://shop.biumfood.com/upload/1623296512image_product044.jpg",
+                "공주",
+                "20:35",
+            ),
+            myInfo = HistoryInfoUiModel(
+                "https://shop.biumfood.com/upload/1623296512image_product044.jpg",
+                "나",
+                "20:35",
+            ),
+            createDate = "4/9",
+        ),
+    )
+}
