bad37e5058de7cceb89299bcafd651aae8df8768
hj1115hj <hyejin_one@naver.com>
2023-06-07 00:51:19 +0900

    feat: historyUiModel 구현

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/history/model/HistoryInfoUiModel.kt
@@ -0,0 +1,7 @@
+package com.mashup.twotoo.presenter.history.model
+
+data class HistoryInfoUiModel(
+    val photoUrl: String,
+    val ownerNickName: String,
+    val createdTime: String,
+)
---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/history/model/HistoryItemUiModel.kt
@@ -0,0 +1,7 @@
+package com.mashup.twotoo.presenter.history.model
+
+data class HistoryItemUiModel(
+    val partnerInfo: HistoryInfoUiModel,
+    val myInfo: HistoryInfoUiModel,
+    val createDate: String,
+)
