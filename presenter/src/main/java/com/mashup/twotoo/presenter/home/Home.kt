538fc8f0da66f9e475c799504dfd8efc8bc416b3
Hyunkuk <boris0815@naver.com>
2023-06-07 16:44:20 +0900

    [feat] : add paperbackground

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/Home.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/Home.kt
@@ -27,65 +27,72 @@ fun HomeScreen(
     onBeeButtonClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.fillMaxSize().background(color = Color(0xFFFCF5E6)),
-    ) {
-        MainToolbar()
-        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-            val (
-                topBar, homeGoalField, goalAchievement, goalCount,
-                homeBackground, beeButton,
-            ) = createRefs()
+    Box(modifier = modifier.fillMaxSize()) {
+        TwoTooImageView(
+            modifier = Modifier.fillMaxSize(),
+            model = R.drawable.image_background,
+            previewPlaceholder = R.drawable.image_background,
+        )
+        Column(
+            modifier = modifier.fillMaxSize(),
+        ) {
+            MainToolbar()
+            ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+                val (
+                    topBar, homeGoalField, goalAchievement, goalCount,
+                    homeBackground, beeButton,
+                ) = createRefs()
 
-            HomeGoalField(
-                modifier = Modifier.width(327.dp).constrainAs(homeGoalField) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                homeGoalFieldData = HomeGoalFieldData(),
-            )
-            HomeGoalAchievement(
-                modifier = Modifier.width(210.dp).height(59.dp).constrainAs(goalAchievement) {
-                    start.linkTo(homeGoalField.start)
-                    top.linkTo(homeGoalField.bottom)
-                }.background(color = Color.White, shape = RoundedCornerShape(15.dp)),
-                goalAchieveDataList = listOf(
-                    HomeGoalAchieveData("공주", 0.7f),
-                    HomeGoalAchieveData("나", 0.6f),
-                ),
-            )
-            HomeGoalCount(
-                modifier = Modifier.constrainAs(goalCount) {
-                    end.linkTo(homeGoalField.end)
-                    top.linkTo(goalAchievement.top)
-                    start.linkTo(goalAchievement.end)
-                    bottom.linkTo(goalAchievement.bottom)
-                },
-                homeGoalCountData = HomeGoalCountData(),
-            )
-            TwoTooImageView(
-                modifier = Modifier.width(380.dp).height(244.dp).constrainAs(homeBackground) {
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                },
-                previewPlaceholder = R.drawable.home_background,
-                model = R.drawable.home_background,
-            )
-            HomeBeeButton(
-                modifier = Modifier.constrainAs(beeButton) {
-                    top.linkTo(homeBackground.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                }.clickable {
-                    onBeeButtonClick()
-                },
-            )
-            HomeShotCountText(
-                homeShotCountTextData = HomeShotCountTextData(),
-            )
+                HomeGoalField(
+                    modifier = Modifier.width(327.dp).constrainAs(homeGoalField) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    homeGoalFieldData = HomeGoalFieldData(),
+                )
+                HomeGoalAchievement(
+                    modifier = Modifier.width(210.dp).height(59.dp).constrainAs(goalAchievement) {
+                        start.linkTo(homeGoalField.start)
+                        top.linkTo(homeGoalField.bottom)
+                    }.background(color = Color.White, shape = RoundedCornerShape(15.dp)),
+                    goalAchieveDataList = listOf(
+                        HomeGoalAchieveData("공주", 0.7f),
+                        HomeGoalAchieveData("나", 0.6f),
+                    ),
+                )
+                HomeGoalCount(
+                    modifier = Modifier.constrainAs(goalCount) {
+                        end.linkTo(homeGoalField.end)
+                        top.linkTo(goalAchievement.top)
+                        start.linkTo(goalAchievement.end)
+                        bottom.linkTo(goalAchievement.bottom)
+                    },
+                    homeGoalCountData = HomeGoalCountData(),
+                )
+                TwoTooImageView(
+                    modifier = Modifier.width(380.dp).height(244.dp).constrainAs(homeBackground) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    previewPlaceholder = R.drawable.image_home_background,
+                    model = R.drawable.image_home_background,
+                )
+                HomeBeeButton(
+                    modifier = Modifier.constrainAs(beeButton) {
+                        top.linkTo(homeBackground.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                    }.clickable {
+                        onBeeButtonClick()
+                    },
+                )
+                HomeShotCountText(
+                    homeShotCountTextData = HomeShotCountTextData(),
+                )
+            }
         }
     }
 }
