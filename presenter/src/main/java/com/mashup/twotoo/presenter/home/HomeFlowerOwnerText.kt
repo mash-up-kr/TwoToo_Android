0ceadc525b2e58e95757eb9b7c1c5c9c17f224ea
Hyunkuk <boris0815@naver.com>
2023-06-08 00:35:35 +0900

    [refactor] : FlowerOwnerText 수정

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeFlowerOwnerText.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeFlowerOwnerText.kt
@@ -1,10 +1,14 @@
 package com.mashup.twotoo.presenter.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 import com.mashup.twotoo.presenter.designsystem.theme.TwotooPink
 import com.mashup.twotoo.presenter.home.UserType.ME
@@ -16,21 +20,29 @@ fun HomeFlowerOwnerText(
     userType: UserType,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        modifier = modifier.background(
-            color = TwoTooTheme.color.backgroundYellow,
-            shape = TwoTooTheme.shape.small,
+    Box(
+        modifier = modifier.then(
+            Modifier.background(
+                color = TwoTooTheme.color.backgroundYellow,
+                shape = TwoTooTheme.shape.small,
+            ).padding(horizontal = 10.dp, vertical = 3.dp),
         ),
-        color = when (userType) {
-            PARTNER -> {
-                TwoTooTheme.color.mainBrown
-            }
-            ME -> {
-                TwotooPink
-            }
-        },
-        text = name,
-    )
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            modifier = Modifier,
+            color = when (userType) {
+                PARTNER -> {
+                    TwoTooTheme.color.mainBrown
+                }
+                ME -> {
+                    TwotooPink
+                }
+            },
+            style = TwoTooTheme.typography.bodyNormal14,
+            text = name,
+        )
+    }
 }
 
 @Preview(showBackground = true)
