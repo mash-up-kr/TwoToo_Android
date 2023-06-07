a8899e836e038442578b41c1ba76b51c0147517d
Hyunkuk <boris0815@naver.com>
2023-06-08 00:34:52 +0900

    [feat] : default value넣기, 이미지 사이즈 enum class 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeModel.kt
@@ -1,18 +1,20 @@
 package com.mashup.twotoo.presenter.home
 
 import androidx.annotation.DrawableRes
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.mashup.twotoo.presenter.R
 
 /**
  * @Created by 김현국 2023/06/07
  */
 
-enum class GrowType(@DrawableRes val growImage: Int) {
-    SEED(growImage = R.drawable.ic_seed),
-    SHOOT(growImage = R.drawable.ic_seed),
-    BUD(growImage = R.drawable.ic_seed),
-    FLOWER(growImage = R.drawable.ic_seed),
-    BLOOM(growImage = R.drawable.ic_seed),
+enum class GrowType(@DrawableRes val growImage: Int, val width: Dp, val height: Dp) {
+    SEED(growImage = R.drawable.ic_seed, width = 25.dp, height = 29.dp),
+    SHOOT(growImage = R.drawable.ic_seed, width = 25.dp, height = 29.dp),
+    BUD(growImage = R.drawable.ic_seed, width = 25.dp, height = 29.dp),
+    FLOWER(growImage = R.drawable.ic_seed, width = 25.dp, height = 29.dp),
+    BLOOM(growImage = R.drawable.ic_seed, width = 25.dp, height = 29.dp),
 }
 
 enum class AuthType {
@@ -27,9 +29,9 @@ enum class UserType {
     ME, PARTNER
 }
 data class HomeGoalAchieveData(
-    val name: String,
-    val type: UserType,
-    val progress: Float,
+    val name: String = "공주",
+    val type: UserType = UserType.PARTNER,
+    val progress: Float = 0.7f,
 )
 
 data class HomeGoalFieldData(
@@ -49,7 +51,37 @@ data class HomeGoalCountData(
 
 data class HomeFlower(
     val name: String = "왕자",
-    val userType: UserType = UserType.PARTNER,
     val growType: GrowType = GrowType.SEED,
     val authType: AuthType = AuthType.FirstCreateChallenge,
+) {
+    companion object {
+        val firstChallengeList = listOf(
+            HomeFlower(),
+            HomeFlower(),
+        )
+        val authOnlyPartnerList = listOf(
+            HomeFlower().copy(authType = AuthType.AuthOnlyPartner),
+            HomeFlower().copy(authType = AuthType.AuthOnlyPartner),
+        )
+        val authOnlyMeList = listOf(
+            HomeFlower().copy(authType = AuthType.AuthOnlyMe),
+            HomeFlower().copy(authType = AuthType.AuthOnlyMe),
+        )
+        val authBoth = listOf(
+            HomeFlower().copy(authType = AuthType.AuthBoth),
+            HomeFlower().copy(authType = AuthType.AuthBoth),
+        )
+        val doNotAuthBoth = listOf(
+            HomeFlower().copy(authType = AuthType.DoNotAuthBoth),
+            HomeFlower().copy(authType = AuthType.DoNotAuthBoth),
+        )
+    }
+}
+
+data class HomeModel(
+    val homeFlower: List<HomeFlower> = HomeFlower.firstChallengeList,
+    val homeGoalAchieveData: HomeGoalAchieveData = HomeGoalAchieveData(),
+    val homeGoalCountData: HomeGoalCountData = HomeGoalCountData(),
+    val homeGoalFieldData: HomeGoalFieldData = HomeGoalFieldData(),
+    val homeShotCountTextData: HomeShotCountTextData = HomeShotCountTextData(),
 )
