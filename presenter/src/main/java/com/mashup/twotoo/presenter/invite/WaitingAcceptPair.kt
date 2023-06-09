2118def6248e665671992a339f7605b12aa58822
ddyeon <cab05102@naver.com>
2023-06-10 00:43:02 +0900

    [fix] 상대방 수락 UI topbar, background 적용

--- presenter/src/main/java/com/mashup/twotoo/presenter/invite/WaitingAcceptPair.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/invite/WaitingAcceptPair.kt
@@ -2,15 +2,20 @@ package com.mashup.twotoo.presenter.invite
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,37 +24,51 @@ import com.mashup.twotoo.presenter.R
 import com.mashup.twotoo.presenter.designsystem.component.TwoTooImageView
 import com.mashup.twotoo.presenter.designsystem.component.button.TwoTooOutlineTextButton
 import com.mashup.twotoo.presenter.designsystem.component.button.TwoTooTextButton
+import com.mashup.twotoo.presenter.designsystem.component.toolbar.TwoTooMainToolbar
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 
 @Composable
 fun WaitingAcceptPair() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(TwoTooTheme.color.backgroundYellow),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Text(
-            modifier = Modifier.padding(top = 150.dp, bottom = 13.dp),
-            text = stringResource(id = R.string.invite_waiting_other),
-            textAlign = TextAlign.Center,
-            style = TwoTooTheme.typography.headLineNormal28,
-            color = TwoTooTheme.color.mainBrown,
-        )
-        Text(
-            text = stringResource(id = R.string.invite_refresh),
-            textAlign = TextAlign.Center,
-            style = TwoTooTheme.typography.bodyNormal16,
-            color = TwoTooTheme.color.gray600,
-        )
-        TwoTooImageView(
-            modifier = Modifier.size(150.dp).padding(top = 10.dp),
-            model = R.drawable.invite_waiting_heart,
-            previewPlaceholder = R.drawable.invite_waiting_heart,
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        WaitingInviteBottom()
+    Scaffold(
+        topBar = { TwoTooMainToolbar() },
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .paint(
+                    painterResource(id = R.drawable.image_background),
+                    contentScale = ContentScale.FillBounds,
+                ),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    modifier = Modifier.padding(top = 150.dp, bottom = 13.dp),
+                    text = stringResource(id = R.string.invite_waiting_other),
+                    textAlign = TextAlign.Center,
+                    style = TwoTooTheme.typography.headLineNormal28,
+                    color = TwoTooTheme.color.mainBrown,
+                )
+                Text(
+                    text = stringResource(id = R.string.invite_refresh),
+                    textAlign = TextAlign.Center,
+                    style = TwoTooTheme.typography.bodyNormal16,
+                    color = TwoTooTheme.color.gray600,
+                )
+                TwoTooImageView(
+                    modifier = Modifier.size(150.dp).padding(top = 10.dp),
+                    model = R.drawable.invite_waiting_heart,
+                    previewPlaceholder = R.drawable.invite_waiting_heart,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                WaitingInviteBottom()
+            }
+        }
     }
 }
 
