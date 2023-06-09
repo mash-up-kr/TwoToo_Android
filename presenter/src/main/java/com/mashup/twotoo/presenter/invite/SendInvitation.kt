ba38271aacfe13b7c17a90148bcb30d5c20ce5f7
ddyeon <cab05102@naver.com>
2023-06-10 00:44:16 +0900

    [fix] 초대장 보내기 ui topbar, background 이미지 적용

--- presenter/src/main/java/com/mashup/twotoo/presenter/invite/SendInvitation.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/invite/SendInvitation.kt
@@ -1,49 +1,68 @@
 package com.mashup.twotoo.presenter.invite
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.twotoo.presenter.R
 import com.mashup.twotoo.presenter.designsystem.component.button.TwoTooTextButton
+import com.mashup.twotoo.presenter.designsystem.component.toolbar.TwoTooMainToolbar
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 
 @Composable
 fun SendInvitation() {
-    Column(
-        modifier = Modifier.fillMaxSize().background(TwoTooTheme.color.backgroundYellow),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround,
-    ) {
-        Text(
-            text = stringResource(id = R.string.invite_title),
-            modifier = Modifier.padding(top = 137.dp, bottom = 24.dp),
-            style = TwoTooTheme.typography.headLineNormal28,
-            color = TwoTooTheme.color.mainBrown,
-            textAlign = TextAlign.Center,
-        )
-        Text(
-            text = stringResource(id = R.string.invite_desc),
-            style = TwoTooTheme.typography.bodyNormal16,
-            color = TwoTooTheme.color.gray600,
-            textAlign = TextAlign.Center,
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        TwoTooTextButton(
-            text = stringResource(id = R.string.send_invite),
-        ) {}
-        Spacer(modifier = Modifier.height(55.dp))
+    Scaffold(
+        topBar = { TwoTooMainToolbar() },
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .paint(
+                    painterResource(id = R.drawable.image_background),
+                    contentScale = ContentScale.FillBounds,
+                ),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.invite_title),
+                    modifier = Modifier.padding(top = 137.dp, bottom = 24.dp),
+                    style = TwoTooTheme.typography.headLineNormal28,
+                    color = TwoTooTheme.color.mainBrown,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = stringResource(id = R.string.invite_desc),
+                    style = TwoTooTheme.typography.bodyNormal16,
+                    color = TwoTooTheme.color.gray600,
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                TwoTooTextButton(
+                    text = stringResource(id = R.string.send_invite),
+                ) {}
+                Spacer(modifier = Modifier.height(55.dp))
+            }
+        }
     }
 }
 
--- presenter/src/main/java/com/mashup/twotoo/presenter/invite/WaitingAcceptPair.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/invite/WaitingAcceptPair.kt
@@ -1,6 +1,5 @@
 package com.mashup.twotoo.presenter.invite
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
