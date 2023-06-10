d09c0e1e055fb8992033894977d9ef9bcac3d062
Hyunkuk <boris0815@naver.com>
2023-06-10 18:03:04 +0900

    [feat]: 현재 destination에 따른 Color 반환 함수 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/ui/TwoTooAppState.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/ui/TwoTooAppState.kt
@@ -3,12 +3,14 @@ package com.mashup.twotoo.presenter.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
+import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 import com.mashup.twotoo.presenter.garden.navigation.GardenNavigationRoute
 import com.mashup.twotoo.presenter.garden.navigation.navigateToGarden
 import com.mashup.twotoo.presenter.home.navigation.HomeNavigationRoute
@@ -48,6 +50,19 @@ class TwoTooAppState(
         }
     val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
 
+    val getContainerColorByDestination: Color
+        @Composable get() = if (currentTopLevelDestination == Home) {
+            TwoTooTheme.color.mainPink
+        } else {
+            TwoTooTheme.color.mainYellow
+        }
+
+    val getUnSelectedColorByDestination: Color
+        @Composable get() = if (currentTopLevelDestination == Home) {
+            TwoTooTheme.color.backgroundYellow
+        } else {
+            TwoTooTheme.color.mainPink
+        }
     fun navigationToTopLevelDestination(topLevelDestination: TopLevelDestination) {
         val topLevelOptions = navOptions {
             popUpTo(navController.graph.findStartDestination().id) {
