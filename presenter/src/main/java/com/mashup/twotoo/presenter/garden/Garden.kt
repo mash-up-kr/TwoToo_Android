66ea99b7fdf6dd38d8a47fbd47ad388d4f45ac10
hj1115hj <hyejin_one@naver.com>
2023-06-10 16:54:20 +0900

    feat: scaffold 적용

--- presenter/src/main/java/com/mashup/twotoo/presenter/garden/Garden.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/garden/Garden.kt
@@ -1,10 +1,17 @@
 package com.mashup.twotoo.presenter.garden
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.designsystem.component.toolbar.TwoTooMainToolbar
+import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 
 @Composable
 fun GardenRoute() {
@@ -13,9 +20,23 @@ fun GardenRoute() {
 
 @Composable
 fun GardenScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
+    Scaffold(
+        topBar = {
+            TwoTooMainToolbar(text = "우리의 정원", onClickHelpIcon = {})
+        },
+        containerColor = TwoTooTheme.color.backgroundYellow,
     ) {
-        Text("Garden Screen")
+        val challenges: List<String> = listOf("하루 30분 운동하기")
+        LazyVerticalGrid(modifier = Modifier.padding(paddingValues = it), columns = GridCells.Adaptive(minSize = 157.dp)) {
+            items(challenges) { challenge ->
+                Text(challenge)
+            }
+        }
     }
+}
+
+@Preview(widthDp = 327, heightDp = 812)
+@Composable
+private fun PreviewGardenScreen() {
+    GardenScreen()
 }
