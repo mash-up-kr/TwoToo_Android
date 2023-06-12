d18c646b8e9934f8b9ee0eb299162c39abb2c4e6
ddyeon <cab05102@naver.com>
2023-06-12 23:59:10 +0900

    [feat] 챌린지 생성 2단계 content

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallengeTwoSetp.kt
@@ -0,0 +1,33 @@
+package com.mashup.twotoo.presenter.createChallenge
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.R
+import com.mashup.twotoo.presenter.designsystem.component.textfield.TwoTooTextField
+
+@Composable
+fun CreateChallengeTwoStep() {
+    var text by remember { mutableStateOf("") }
+    TwoTooTextField(
+        modifier = Modifier.fillMaxWidth().padding(top = 46.dp).height(253.dp),
+        text = text,
+        textHint = stringResource(id = R.string.input_challenge_info_placeholder),
+        updateText = { text = it },
+    )
+}
+
+@Composable
+@Preview
+private fun PreviewCreateChallengeTwoStep() {
+    CreateChallengeTwoStep()
+}
