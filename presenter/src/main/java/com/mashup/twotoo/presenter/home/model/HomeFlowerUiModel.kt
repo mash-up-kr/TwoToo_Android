4d6d307bef4d1d9f27625185674a74b5fac5f566
Hyunkuk <boris0815@naver.com>
2023-06-13 17:39:49 +0900

    [refactor] : list였던 partner와 me field로 구별

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/model/HomeFlowerUiModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/model/HomeFlowerUiModel.kt
@@ -4,10 +4,37 @@ package com.mashup.twotoo.presenter.home.model
  * @Created by 김현국 2023/06/09
  */
 
+data class HomeFlowerPartnerAndMeUiModel(
+    val partner: HomeFlowerUiModel,
+    val me: HomeFlowerUiModel,
+) {
+    companion object {
+        val firstChallenge = HomeFlowerPartnerAndMeUiModel(
+            partner = HomeFlowerUiModel.default,
+            me = HomeFlowerUiModel.default,
+        )
+        val authOnlyPartner = HomeFlowerPartnerAndMeUiModel(
+            partner = HomeFlowerUiModel.default.copy(authType = AuthType.AuthOnlyPartner),
+            me = HomeFlowerUiModel.default.copy(authType = AuthType.AuthOnlyPartner),
+        )
+        val authOnlyMe = HomeFlowerPartnerAndMeUiModel(
+            partner = HomeFlowerUiModel.default.copy(authType = AuthType.AuthOnlyMe),
+            me = HomeFlowerUiModel.default.copy(authType = AuthType.AuthOnlyMe),
+        )
+        val authBoth = HomeFlowerPartnerAndMeUiModel(
+            partner = HomeFlowerUiModel.default.copy(authType = AuthType.AuthBoth),
+            me = HomeFlowerUiModel.default.copy(authType = AuthType.AuthBoth),
+        )
+        val doNotAuthBoth = HomeFlowerPartnerAndMeUiModel(
+            partner = HomeFlowerUiModel.default.copy(authType = AuthType.DoNotAuthBoth),
+            me = HomeFlowerUiModel.default.copy(authType = AuthType.DoNotAuthBoth),
+        )
+    }
+}
 data class HomeFlowerUiModel(
-    val name: String = "왕자",
-    val growType: GrowType = GrowType.SEED,
-    val authType: AuthType = AuthType.FirstCreateChallenge,
+    val name: String,
+    val growType: GrowType,
+    val authType: AuthType,
 ) {
     companion object {
 
@@ -15,27 +42,6 @@ data class HomeFlowerUiModel(
             name = "왕자",
             growType = GrowType.SEED,
             authType = AuthType.FirstCreateChallenge,
-        )
-
-        val firstChallengeList = listOf(
-            HomeFlowerUiModel(),
-            HomeFlowerUiModel(),
-        )
-        val authOnlyPartnerList = listOf(
-            HomeFlowerUiModel().copy(authType = AuthType.AuthOnlyPartner),
-            HomeFlowerUiModel().copy(authType = AuthType.AuthOnlyPartner),
-        )
-        val authOnlyMeList = listOf(
-            HomeFlowerUiModel().copy(authType = AuthType.AuthOnlyMe),
-            HomeFlowerUiModel().copy(authType = AuthType.AuthOnlyMe),
-        )
-        val authBoth = listOf(
-            HomeFlowerUiModel().copy(authType = AuthType.AuthBoth),
-            HomeFlowerUiModel().copy(authType = AuthType.AuthBoth),
-        )
-        val doNotAuthBoth = listOf(
-            HomeFlowerUiModel().copy(authType = AuthType.DoNotAuthBoth),
-            HomeFlowerUiModel().copy(authType = AuthType.DoNotAuthBoth),
         )
     }
 }
