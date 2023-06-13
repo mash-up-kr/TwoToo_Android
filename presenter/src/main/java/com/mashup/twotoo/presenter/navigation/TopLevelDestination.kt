2782d2c7e3bbc1394a78891e42de5c052894fbbf
Hyunkuk <boris0815@naver.com>
2023-06-14 00:37:37 +0900

    [refactor] : rename buttonTitleTextId

--- presenter/src/main/java/com/mashup/twotoo/presenter/navigation/TopLevelDestination.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/navigation/TopLevelDestination.kt
@@ -8,21 +8,21 @@ import com.mashup.twotoo.presenter.designsystem.icon.TwoTooIcon
 enum class TopLevelDestination(
     val icon: Icon,
     val iconTextId: Int,
-    val titleTextId: Int,
+    val buttonTitleTextId: Int,
 ) {
     Garden(
         icon = DrawableResourceIcon(TwoTooIcon.GardenIcon),
         iconTextId = R.string.garden,
-        titleTextId = R.string.garden,
+        buttonTitleTextId = R.string.garden_nav_button,
     ),
     Home(
         icon = DrawableResourceIcon(TwoTooIcon.HomeIcon),
         iconTextId = R.string.home,
-        titleTextId = R.string.home,
+        buttonTitleTextId = R.string.home_nav_button,
     ),
     User(
         icon = DrawableResourceIcon(TwoTooIcon.UserIcon),
         iconTextId = R.string.user,
-        titleTextId = R.string.user,
+        buttonTitleTextId = R.string.user_nav_button,
     ),
 }
