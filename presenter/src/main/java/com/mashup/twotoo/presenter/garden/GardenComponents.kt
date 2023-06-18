45be8ac1bcfddcf5ea31d4891d29b2fda02da671
hj1115hj <hyejin_one@naver.com>
2023-06-18 21:10:48 +0900

    feat: 정원 꽃 이미지 변경

--- presenter/src/main/java/com/mashup/twotoo/presenter/garden/GardenComponents.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/garden/GardenComponents.kt
@@ -1,6 +1,5 @@
 package com.mashup.twotoo.presenter.garden
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.LocalTextStyle
@@ -11,10 +10,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.twotoo.presenter.R
+import com.mashup.twotoo.presenter.designsystem.component.TwoTooImageView
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooRound6
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 import com.mashup.twotoo.presenter.garden.model.ChallengeCardInfoUiModel
@@ -25,11 +24,11 @@ fun ChallengeCard(challengeCardInfoUiModel: ChallengeCardInfoUiModel) {
         modifier = Modifier.height(216.dp).width(156.dp).clip(TwoTooRound6).background(TwoTooTheme.color.mainWhite),
     ) {
         ChallengeInfo(challengeCardInfoUiModel)
-        Image(
+        TwoTooImageView(
+            model = R.drawable.challenge_card_ground,
             modifier = Modifier.height(58.dp).fillMaxWidth().align(Alignment.BottomStart),
-            painter = painterResource(id = R.drawable.challenge_card_ground),
-            contentDescription = null,
             contentScale = ContentScale.Crop,
+            previewPlaceholder = R.drawable.challenge_card_ground,
         )
         // Todo 꽃 선택하는 ui에서 사용하는 model 공통으로 사용하도록 해야함
         Flowers()
@@ -54,18 +53,19 @@ private fun ChallengeInfo(challengeCardInfoUiModel: ChallengeCardInfoUiModel) {
 @Composable
 private fun BoxScope.Flowers() {
     Row(
-        modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 46.dp),
+        modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 23.dp),
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.tulip),
-            contentDescription = null,
+        TwoTooImageView(
+            modifier = Modifier.size(68.dp),
+            model = R.drawable.flower_fig,
             contentScale = ContentScale.Fit,
+            previewPlaceholder = R.drawable.flower_fig,
         )
-        Image(
-            modifier = Modifier.padding(start = 15.dp),
-            painter = painterResource(id = R.drawable.lose),
-            contentDescription = null,
+        TwoTooImageView(
+            modifier = Modifier.size(68.dp),
+            model = R.drawable.flower_chrysanthemum,
             contentScale = ContentScale.Fit,
+            previewPlaceholder = R.drawable.flower_chrysanthemum,
         )
     }
 }
