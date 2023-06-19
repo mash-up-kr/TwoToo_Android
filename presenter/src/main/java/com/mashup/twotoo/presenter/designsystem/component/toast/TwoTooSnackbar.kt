8ab09e0e0148e4ab270b2a94cfc089b45a0cdeaa
ddyeon <cab05102@naver.com>
2023-06-19 22:39:20 +0900

    [feat] TwoTooSnackbar Custom

--- presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/toast/TwoTooSnackbar.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/toast/TwoTooSnackbar.kt
@@ -1,75 +1,85 @@
 package com.mashup.twotoo.presenter.designsystem.component.toast
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.R
+import com.mashup.twotoo.presenter.designsystem.theme.MainLightPink
+import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 import kotlinx.coroutines.launch
 
-@Preview
 @Composable
-fun TwoTooSnackBarTest() {
-    val snackState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
-    val scaffoldState = rememberScaffoldState()
-
-    Scaffold(
-        modifier = Modifier,
-        scaffoldState = scaffoldState,
-        snackbarHost = {
-            SnackbarHost(
-                hostState = snackState,
-            ) { snackData ->
-                TwoTooSnackBarView(snackData.message)
-            }
-        },
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
-        ) {
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {
-                    coroutineScope.launch {
-                        snackState.showSnackbar("Custom Snackbar")
-                    }
-                },
-            ) {
-                Text("Show Snackbar")
-            }
-        }
+fun SnackBarHost(modifier: Modifier, snackState: SnackbarHostState) {
+    SnackbarHost(
+        modifier = modifier,
+        hostState = snackState,
+    ) { snackData ->
+        TwoTooSnackBarView(snackData.message)
     }
 }
 
 @Composable
-fun TwoTooSnackBarView(message: String) {
+fun TwoTooSnackBarView(
+    message: String
+) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-        shape = RoundedCornerShape(10.dp),
-        backgroundColor = Color.Yellow,
+        shape = TwoTooTheme.shape.extraSmall,
+        backgroundColor = MainLightPink,
     ) {
         Text(
             text = message,
-            modifier = Modifier.padding(10.dp, 10.dp),
+            modifier = Modifier.padding(13.dp, 12.dp),
+            style = TwoTooTheme.typography.bodyNormal16,
             textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewTwoTooSnackBar() {
+    val snackState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    coroutineScope.launch {
+                        snackState.showSnackbar(
+                            context.getString(R.string.other_not_accept),
+                        )
+                    }
+                },
+            ) {
+                Text("Preview Snackbar")
+            }
+        }
+        SnackBarHost(
+            Modifier.align(Alignment.BottomCenter),
+            snackState,
         )
     }
 }
