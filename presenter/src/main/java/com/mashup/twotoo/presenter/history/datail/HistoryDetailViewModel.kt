606bdd4bd72c4a209c9b91fb97efc628be9acdfe
hj1115hj <hyejin_one@naver.com>
2023-06-25 19:56:11 +0900

    feat: history detail mvi 적용

---
+++ presenter/src/main/java/com/mashup/twotoo/presenter/history/datail/HistoryDetailViewModel.kt
@@ -0,0 +1,26 @@
+package com.mashup.twotoo.presenter.history.datail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.twotoo.presenter.history.datail.model.HistoryDetailInfoUiModel
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+
+class HistoryDetailViewModel() : ContainerHost<HistoryDetailState, Unit>, ViewModel() {
+    override val container: Container<HistoryDetailState, Unit> = container(HistoryDetailState(null))
+
+    init {
+        loadHistoryDetail()
+    }
+
+    private fun loadHistoryDetail() = intent {
+        viewModelScope.launch {
+            val newHistoryDetailInfo = HistoryDetailInfoUiModel.getHistoryDetailInfoUiModelToPreview()
+            reduce { state.copy(historyDetailInfo = newHistoryDetailInfo) }
+        }
+    }
+}
