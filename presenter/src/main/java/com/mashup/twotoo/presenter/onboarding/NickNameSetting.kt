aaa7e95b07152c0a990413092ecdab336f53fd26
ddyeon <cab05102@naver.com>
2023-06-26 22:04:40 +0900

    [fix] 닉네임 설정 화면 이미지 추가, 로직 수정

--- presenter/src/main/java/com/mashup/twotoo/presenter/onboarding/NickNameSetting.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/onboarding/NickNameSetting.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,6 +31,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.twotoo.presenter.R
+import com.mashup.twotoo.presenter.designsystem.component.TwoTooImageView
 import com.mashup.twotoo.presenter.designsystem.component.button.TwoTooTextButton
 import com.mashup.twotoo.presenter.designsystem.component.textfield.TwoTooTextField
 import com.mashup.twotoo.presenter.designsystem.component.toolbar.TwoTooMainToolbar
@@ -67,8 +69,20 @@ fun NickNameSetting(
                 verticalArrangement = Arrangement.SpaceBetween,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                otherNickName?.let { nickname ->
-                    InviteGuide(nickname)
+                if (otherNickName != null) {
+                    TwoTooImageView(
+                        modifier = Modifier.size(97.dp, 85.dp),
+                        previewPlaceholder = R.drawable.img_nickname_mate,
+                        model = R.drawable.img_nickname_mate,
+                        contentScale = ContentScale.Crop,
+                    )
+                    InviteGuide(otherNickName)
+                } else {
+                    TwoTooImageView(
+                        modifier = Modifier.size(149.dp, 129.dp),
+                        previewPlaceholder = R.drawable.img_nicknam_my,
+                        model = R.drawable.img_nicknam_my,
+                    )
                 }
                 Text(
                     modifier = Modifier.padding(top = 78.dp),
@@ -157,5 +171,5 @@ private fun InviteGuidePreview() {
 @Preview
 @Composable
 private fun NickNameSettingPreview() {
-    NickNameSetting({},"공주")
+    NickNameSetting({}, "공주")
 }
