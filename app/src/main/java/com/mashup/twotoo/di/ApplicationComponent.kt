a807bda4c653309ce12d128429344150f4c0281f
Hyunkuk <boris0815@naver.com>
2023-06-27 03:05:26 +0900

    [feat] : add context, remove Module

--- app/src/main/java/com/mashup/twotoo/di/ApplicationComponent.kt
+++ app/src/main/java/com/mashup/twotoo/di/ApplicationComponent.kt
@@ -1,10 +1,11 @@
 package com.mashup.twotoo.di
 
-import com.mashup.twotoo.presenter.MainActivity
+import android.content.Context
 import com.mashup.twotoo.presenter.di.ScreenComponent
 import com.mashup.twotoo.presenter.di.ViewModelFactoryModule
 import com.mashup.twotoo.presenter.di.ViewModelModule
 import com.mashup.twotoo.presenter.home.HomeComponent
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
@@ -16,16 +17,13 @@ import javax.inject.Singleton
         DataStoreModule::class,
         RepositoryModule::class,
         ScreenComponent::class,
-        ViewModelFactoryModule::class,
-        ViewModelModule::class,
     ],
 )
 interface ApplicationComponent {
 
     @Component.Factory
     interface Factory {
-        fun create(): ApplicationComponent
+        fun create(@BindsInstance context: Context): ApplicationComponent
     }
-    fun inject(mainActivity: MainActivity)
     fun homeComponent(): HomeComponent.Factory
 }
