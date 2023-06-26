bb457e0953fff0c5d3ce67acab03f222d638f731
Hyunkuk <boris0815@naver.com>
2023-06-27 03:09:05 +0900

    [feat] : add TokenUseCase

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeViewModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeViewModel.kt
@@ -3,12 +3,15 @@ package com.mashup.twotoo.presenter.home
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import usecase.token.GetAccessTokenUseCase
 import javax.inject.Inject
 
 /**
  * @Created by 김현국 2023/06/23
  */
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getAccessTokenUseCase: GetAccessTokenUseCase,
+) : ViewModel() {
 
     private val _count = MutableStateFlow(0)
     val count = _count.asStateFlow()
