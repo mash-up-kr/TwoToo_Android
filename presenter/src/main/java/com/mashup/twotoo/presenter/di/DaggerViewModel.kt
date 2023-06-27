a3e6f797fbefd35151519276d70ed9f2e39e3675
Hyunkuk <boris0815@naver.com>
2023-06-27 23:19:33 +0900

    [feat] : remove viewModelStoreOwner

--- presenter/src/main/java/com/mashup/twotoo/presenter/di/DaggerViewModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/di/DaggerViewModel.kt
@@ -3,20 +3,18 @@ package com.mashup.twotoo.presenter.di
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 /**
  * @Created by 김현국 2023/06/23
  */
 
+@Suppress("UNCHECKED_CAST")
 @Composable
 inline fun <reified T : ViewModel> daggerViewModel(
-    viewModelStoreOwner: ViewModelStoreOwner,
     crossinline viewModelInstanceCreator: () -> T,
 ): T =
     viewModel(
-        viewModelStoreOwner = viewModelStoreOwner,
         modelClass = T::class.java,
         key = T::class.simpleName,
         factory = object : ViewModelProvider.Factory {
