82f3579a933e185812ec8d22d7998f84642fd3b6
Hyunkuk <boris0815@naver.com>
2023-06-27 21:28:54 +0900

    [feat] : add Graph Module

--- presenter/src/main/java/com/mashup/twotoo/presenter/di/ScreenModule.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/di/ScreenModule.kt
@@ -1,10 +1,12 @@
 package com.mashup.twotoo.presenter.di
 
+import com.mashup.twotoo.presenter.home.di.HomeComponent
+import com.mashup.twotoo.presenter.mypage.di.UserComponent
 import dagger.Module
 
 /**
  * @Created by 김현국 2023/06/23
  */
 
-@Module(subcomponents = [ViewModelFactoryComponent::class])
+@Module(subcomponents = [HomeComponent::class, UserComponent::class])
 class ScreenModule
