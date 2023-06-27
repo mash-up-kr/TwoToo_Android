35212f53395a83ce1972e9436d566f5dccb9bd45
Hyunkuk <boris0815@naver.com>
2023-06-27 16:10:54 +0900

    [feat]: ViewModelFactoryModule이 서브 컴포넌트로 Screen을 가지도록 변경

--- presenter/src/main/java/com/mashup/twotoo/presenter/di/ViewModelFactoryModule.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/di/ViewModelFactoryModule.kt
@@ -1,10 +1,13 @@
 package com.mashup.twotoo.presenter.di
 
 import androidx.lifecycle.ViewModelProvider
+import com.mashup.twotoo.presenter.home.HomeComponent
 import dagger.Binds
 import dagger.Module
 
-@Module
+@Module(
+    subcomponents = [HomeComponent::class],
+)
 abstract class ViewModelFactoryModule {
 
     @Binds
