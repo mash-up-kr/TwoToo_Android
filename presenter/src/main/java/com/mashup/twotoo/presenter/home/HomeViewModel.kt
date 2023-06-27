5a6e7d49d76f49e173e9e0010bea633c4c93b3dd
Hyunkuk <boris0815@naver.com>
2023-06-27 21:32:27 +0900

    [feat]: viewModel scope추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeViewModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.mashup.twotoo.presenter.home
 
 import androidx.lifecycle.ViewModel
+import com.mashup.twotoo.presenter.home.di.HomeScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import usecase.token.GetAccessTokenUseCase
@@ -9,6 +10,8 @@ import javax.inject.Inject
 /**
  * @Created by 김현국 2023/06/23
  */
+
+@HomeScope
 class HomeViewModel @Inject constructor(
     private val getAccessTokenUseCase: GetAccessTokenUseCase,
 ) : ViewModel() {
