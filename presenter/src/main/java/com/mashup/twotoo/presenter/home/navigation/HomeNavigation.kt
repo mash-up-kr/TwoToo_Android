63ffd6ec7ca638b89103d8a72b349b8c7544749b
Hyunkuk <boris0815@naver.com>
2023-06-27 21:31:01 +0900

    [feat]: add graph navigation

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/navigation/HomeNavigation.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/navigation/HomeNavigation.kt
@@ -3,33 +3,32 @@ package com.mashup.twotoo.presenter.home.navigation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.mashup.twotoo.presenter.di.daggerViewModel
 import com.mashup.twotoo.presenter.home.HomeRoute
-import com.mashup.twotoo.presenter.home.HomeViewModel
+import com.mashup.twotoo.presenter.home.di.HomeComponentProvider
 
 const val HomeNavigationRoute = "home_route"
 
 fun NavController.navigateToHome(navOptions: NavOptions? = null) {
     this.navigate(route = HomeNavigationRoute, navOptions = navOptions)
 }
-fun NavGraphBuilder.homeGraph(
-    factory: ViewModelProvider.Factory,
-) {
+fun NavGraphBuilder.homeGraph() {
     composable(route = HomeNavigationRoute) {
-        val homeViewModel = daggerViewModel<HomeViewModel>(
-            factory = factory,
-        )
+        val homeComponent = (LocalContext.current.applicationContext as HomeComponentProvider).provideHomeComponent()
+        val homeViewModel = daggerViewModel(viewModelStoreOwner = it) {
+            homeComponent.getViewModel()
+        }
+        println("homeViewModel instance : ${homeViewModel.hashCode()}")
         val state = homeViewModel.count.collectAsState()
-        homeViewModel.printHashCode()
         HomeRoute(
+            state = state.value,
             modifier = Modifier.fillMaxSize(),
             onBeeButtonClick = homeViewModel::updateCount,
-            count = state.value,
         )
     }
 }
--- presenter/src/main/java/com/mashup/twotoo/presenter/mypage/navigation/UserNavigation.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/mypage/navigation/UserNavigation.kt
@@ -1,10 +1,15 @@
 package com.mashup.twotoo.presenter.mypage.navigation
 
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.mashup.twotoo.presenter.di.daggerViewModel
 import com.mashup.twotoo.presenter.mypage.MyPageRoute
+import com.mashup.twotoo.presenter.mypage.di.UserComponentProvider
+import javax.inject.Inject
 
 const val UserNavigationRoute = "user_route"
 
@@ -14,6 +19,13 @@ fun NavController.navigateToUser(navOptions: NavOptions? = null) {
 
 fun NavGraphBuilder.userGraph() {
     composable(route = UserNavigationRoute) {
+        val userComponent = (LocalContext.current.applicationContext as UserComponentProvider).provideUserComponent()
+        val userViewModel = daggerViewModel(viewModelStoreOwner = it) {
+            userComponent.getViewModel()
+        }
+        println("userViewModel instance : ${userViewModel.hashCode()}")
         MyPageRoute()
     }
 }
+
+class UserViewModel @Inject constructor() : ViewModel()
