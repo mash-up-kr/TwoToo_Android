4e4efdf496e130e36f89ce857526cf41b20aca22
Hyunkuk <boris0815@naver.com>
2023-07-05 14:41:15 +0900

    [feat]: create challenge Api

---
+++ data/src/main/java/com/mashup/twotoo/datasource/remote/challenge/ChallengeApi.kt
@@ -0,0 +1,40 @@
+package com.mashup.twotoo.datasource.remote.challenge
+
+import com.mashup.twotoo.datasource.remote.challenge.request.ApproveChallengeRequest
+import com.mashup.twotoo.datasource.remote.challenge.request.CreateChallengeRequest
+import com.mashup.twotoo.datasource.remote.challenge.response.Challenge
+import com.mashup.twotoo.datasource.remote.challenge.response.HomeViewResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+/**
+ * @Created by 김현국 2023/07/04
+ */
+interface ChallengeApi {
+
+    @GET("/view/home")
+    suspend fun getHomeState(): HomeViewResponse
+
+    @POST("/challenge")
+    suspend fun createChallenge(
+        @Body createChallengeRequest: CreateChallengeRequest,
+    ): Challenge
+
+    @GET("/challenge")
+    suspend fun getAllChallenge(): List<Challenge>
+
+    @GET("/challenge/{challengeNo}")
+    suspend fun getChallengeByNo(@Path("challengeNo") challengeNo: Long): Challenge
+
+    @DELETE("/challenge/{challengeNo}")
+    suspend fun deleteChallengeByNo(@Path("challengeNo") challengeNo: Long)
+
+    @POST("/challenge/{challengeNo}/approve")
+    suspend fun approveChallengeWithNo(
+        @Path("challengeNo") challengeNo: Long,
+        @Body approveChallengeRequest: ApproveChallengeRequest,
+    ): Challenge
+}
