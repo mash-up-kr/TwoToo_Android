661eed42cbc0b10f8c613f918ee4a86403b4a040
Hyunkuk <boris0815@naver.com>
2023-07-05 15:37:54 +0900

    [feat] : create apply mvi in viewModel

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeViewModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/HomeViewModel.kt
@@ -1,10 +1,20 @@
 package com.mashup.twotoo.presenter.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashup.twotoo.presenter.designsystem.component.bottomsheet.BottomSheetData
 import com.mashup.twotoo.presenter.home.di.HomeScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import usecase.token.GetAccessTokenUseCase
+import com.mashup.twotoo.presenter.home.model.BeforeChallengeState
+import com.mashup.twotoo.presenter.home.model.ChallengeStateTypeUiModel
+import com.mashup.twotoo.presenter.home.model.HomeSideEffect
+import com.mashup.twotoo.presenter.home.model.OngoingChallengeUiModel
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.Container
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
+import org.orbitmvi.orbit.viewmodel.container
+import usecase.challenge.GetHomeViewChallengeStateUseCase
 import javax.inject.Inject
 
 /**
@@ -13,16 +23,77 @@ import javax.inject.Inject
 
 @HomeScope
 class HomeViewModel @Inject constructor(
-    private val getAccessTokenUseCase: GetAccessTokenUseCase,
-) : ViewModel() {
+    private val getHomeViewChallengeStateUseCase: GetHomeViewChallengeStateUseCase,
+) : ViewModel(), ContainerHost<ChallengeStateTypeUiModel, HomeSideEffect> {
 
-    private val _count = MutableStateFlow(0)
-    val count = _count.asStateFlow()
-    fun printHashCode() {
-        println("로그 Two Too : ${this.hashCode()}")
+    override val container: Container<ChallengeStateTypeUiModel, HomeSideEffect> = container(OngoingChallengeUiModel.default)
+
+    fun getHomeViewChallenge() {
+        viewModelScope.launch {
+            getHomeViewChallengeStateUseCase()
+        }
     }
 
-    fun updateCount() {
-        _count.value++
+    fun navigateToChallengeDetail() = intent {
+        postSideEffect(HomeSideEffect.NavigateToChallengeDetail)
+    }
+
+    fun openToShotBottomSheet() = intent {
+        postSideEffect(HomeSideEffect.OpenToShotBottomSheet)
+    }
+
+    fun onClickBeforeChallengeTextButton(beforeChallengeState: BeforeChallengeState) = intent {
+        // TODO create Challenge navigation 연결
+        when (beforeChallengeState) {
+            BeforeChallengeState.EMPTY -> {
+                postSideEffect(HomeSideEffect.NavigationToCreateChallenge)
+            }
+            BeforeChallengeState.REQUEST -> {
+                // Todo 챌린지 확인 페이지 이동
+            }
+            BeforeChallengeState.RESPONSE -> {
+                // Todo 챌린지 확인 페이지 이동 Step3
+            }
+            BeforeChallengeState.WAIT -> {
+                // Todo 챌린지 확인 페이지 이동
+            }
+            BeforeChallengeState.TERMINATION -> {
+                postSideEffect(HomeSideEffect.NavigationToCreateChallenge)
+            }
+        }
+    }
+
+    fun onClickSendBottomSheetDataButton(bottomSheetData: BottomSheetData) = intent {
+        when (bottomSheetData) {
+            is BottomSheetData.AuthenticateData -> {
+                // 서버 데이터 전송 sideEffect
+
+                // toast sideEffect
+                // Todo 텍스트 string resource로 변경
+                HomeSideEffect.Toast(
+                    "오늘의 인증을 완료했습니다.",
+                )
+            }
+            is BottomSheetData.ShotData -> {
+                // 서버 데이터 전송
+
+                // toast sideEffect
+                postSideEffect(
+                    HomeSideEffect.Toast(
+                        "상대방에게 찌르기 문구가 보내졌습니다.",
+                    ),
+                )
+            }
+            is BottomSheetData.CheerData -> {
+                // 서버 데이터 전송
+
+                // toast sideEffect
+                postSideEffect(
+                    HomeSideEffect.Toast(
+                        "칭찬 한마디가 등록되었습니다.",
+                    ),
+                )
+            }
+        }
     }
 }
