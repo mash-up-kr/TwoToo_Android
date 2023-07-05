f3716a61cfe71433dc0eae41742a792ab9b0805f
Hyunkuk <boris0815@naver.com>
2023-07-05 13:31:08 +0900

    [feat] : add dagger api module

---
+++ data/src/main/java/com/mashup/twotoo/di/ApiModule.kt
@@ -0,0 +1,20 @@
+package com.mashup.twotoo.di
+
+import com.mashup.twotoo.datasource.remote.challenge.ChallengeApi
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+/**
+ * @Created by 김현국 2023/07/04
+ */
+
+@Module
+class ApiModule {
+    @Provides
+    @Singleton
+    fun provideChallengeApi(retrofit: Retrofit): ChallengeApi {
+        return retrofit.create(ChallengeApi::class.java)
+    }
+}
