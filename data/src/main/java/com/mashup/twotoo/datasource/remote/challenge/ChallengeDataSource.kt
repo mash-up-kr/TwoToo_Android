ea893d35b94bb73e57e009986bff728d1b40aa1c
Hyunkuk <boris0815@naver.com>
2023-07-06 13:02:04 +0900

    [feat]: add logic to challenge data source

--- data/src/main/java/com/mashup/twotoo/datasource/remote/challenge/ChallengeDataSource.kt
+++ data/src/main/java/com/mashup/twotoo/datasource/remote/challenge/ChallengeDataSource.kt
@@ -1,6 +1,8 @@
 package com.mashup.twotoo.datasource.remote.challenge
 
-import com.mashup.twotoo.datasource.remote.challenge.response.HomeViewResponse
+import com.mashup.twotoo.datasource.remote.challenge.request.ApproveChallengeRequest
+import com.mashup.twotoo.datasource.remote.challenge.request.CreateChallengeRequest
+import com.mashup.twotoo.datasource.remote.challenge.response.Challenge
 import javax.inject.Inject
 
 /**
@@ -9,7 +11,35 @@ import javax.inject.Inject
 class ChallengeDataSource @Inject constructor(
     private val challengeApi: ChallengeApi,
 ) {
-    suspend fun getHomeState(): HomeViewResponse {
-        return challengeApi.getHomeState()
+    suspend fun createChallenge(
+        createChallengeRequest: CreateChallengeRequest,
+    ): Challenge {
+        return challengeApi.createChallenge(createChallengeRequest = createChallengeRequest)
+    }
+
+    suspend fun getAllChallenge(): List<Challenge> {
+        return challengeApi.getAllChallenge()
+    }
+
+    suspend fun getChallengeByNo(
+        challengeNo: Long,
+    ): Challenge {
+        return challengeApi.getChallengeByNo(challengeNo = challengeNo)
+    }
+
+    suspend fun deleteChallengeByNo(
+        challengeNo: Long,
+    ) {
+        challengeApi.deleteChallengeByNo(challengeNo = challengeNo)
+    }
+
+    suspend fun approveChallengeWithNo(
+        challengeNo: Long,
+        approveChallengeRequest: ApproveChallengeRequest,
+    ): Challenge {
+        return challengeApi.approveChallengeWithNo(
+            challengeNo = challengeNo,
+            approveChallengeRequest = approveChallengeRequest,
+        )
     }
 }
