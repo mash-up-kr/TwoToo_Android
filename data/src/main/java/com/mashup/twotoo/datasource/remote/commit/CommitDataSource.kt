5843e5e55bb58e51df1a33755199f119d518b9c1
Hyunkuk <boris0815@naver.com>
2023-07-10 16:30:24 +0900

    [feat] : create datasource

---
+++ data/src/main/java/com/mashup/twotoo/datasource/remote/commit/CommitDataSource.kt
@@ -0,0 +1,30 @@
+package com.mashup.twotoo.datasource.remote.commit
+
+import com.mashup.twotoo.datasource.remote.commit.request.CommitRequest
+import com.mashup.twotoo.datasource.remote.commit.response.Commit
+import javax.inject.Inject
+
+/**
+ * @Created by 김현국 2023/07/10
+ */
+class CommitDataSource @Inject constructor(
+    private val commitApi: CommitApi,
+) {
+    suspend fun commit(
+        commitRequest: CommitRequest,
+    ): Commit {
+        return commitApi.commit(commitRequest = commitRequest)
+    }
+
+    suspend fun getCommitByNo(
+        commitNo: String,
+    ): Commit {
+        return commitApi.getCommitByNo(commitNo = commitNo)
+    }
+
+    suspend fun cheerByNo(
+        commitNo: String,
+    ): Commit {
+        return commitApi.cheerByNo(commitNo = commitNo)
+    }
+}
