49e6c2843ff3f578c064d072c4240ccd654b4bf0
Hyunkuk <boris0815@naver.com>
2023-07-10 17:42:07 +0900

    [refactor] : change type

--- data/src/main/java/com/mashup/twotoo/datasource/remote/challenge/ChallengeApi.kt
+++ data/src/main/java/com/mashup/twotoo/datasource/remote/challenge/ChallengeApi.kt
@@ -20,14 +20,14 @@ interface ChallengeApi {
     suspend fun getAllChallenge(): List<Challenge>
 
     @GET("/challenge/{challengeNo}")
-    suspend fun getChallengeByNo(@Path("challengeNo") challengeNo: Long): Challenge
+    suspend fun getChallengeByNo(@Path("challengeNo") challengeNo: Int): Challenge
 
     @DELETE("/challenge/{challengeNo}")
-    suspend fun deleteChallengeByNo(@Path("challengeNo") challengeNo: Long)
+    suspend fun deleteChallengeByNo(@Path("challengeNo") challengeNo: Int): Int
 
     @POST("/challenge/{challengeNo}/approve")
     suspend fun approveChallengeWithNo(
-        @Path("challengeNo") challengeNo: Long,
+        @Path("challengeNo") challengeNo: Int,
         @Body approveChallengeRequest: ApproveChallengeRequest,
     ): Challenge
 }
