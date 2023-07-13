bf9843ace49df76109cf8eb10a2d0bc78dde438c
ddyeon <cab05102@naver.com>
2023-07-13 21:18:43 +0900

    [fix] add datastore usernumber, remove refreshtoken

--- domain/src/main/java/repository/UserDataStoreRepository.kt
+++ domain/src/main/java/repository/UserDataStoreRepository.kt
@@ -3,8 +3,6 @@ package repository
 interface UserDataStoreRepository {
     suspend fun getAccessToken(): String
     suspend fun setAccessToken(accessToken: String)
-    suspend fun getKaKaoAccessToken(): String
-    suspend fun setKaKaoAccessToken(kaKaoAccessToken: String)
-    suspend fun getKaKaoRefreshToken(): String
-    suspend fun setKaKaoRefreshToken(kaKaoRefreshToken: String)
+    suspend fun setUserNo(userNo: Int)
+    suspend fun getUserNo(): Int
 }
