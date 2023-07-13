37b14c52023b5308468e06fcec54a8329abf4fed
Hyunkuk <boris0815@naver.com>
2023-07-13 18:25:17 +0900

    [feat] : add domain default data

--- domain/src/main/java/model/challenge/response/HomeViewResponseDomainModel.kt
+++ domain/src/main/java/model/challenge/response/HomeViewResponseDomainModel.kt
@@ -6,4 +6,37 @@ data class HomeViewResponseDomainModel(
     val user1Commit: UserCommitResponseDomainModel?,
     val user2Commit: UserCommitResponseDomainModel?,
     val viewState: String,
-)
+) {
+    companion object {
+
+        val default = HomeViewResponseDomainModel(
+            viewState = "BEFORE_CREATE",
+            onGoingChallenge = ChallengeResponseDomainModel.default,
+            user1Commit = UserCommitResponseDomainModel.default,
+            user2Commit = UserCommitResponseDomainModel.default.copy(
+                userNo = 2,
+            ),
+            challengeTotal = 1,
+        )
+
+        val beforeCreate = default
+        val beforeMyApprove = default.copy(
+            viewState = "BEFORE_MY_APPROVE",
+        )
+        val beforePartnerApprove = default.copy(
+            viewState = "BEFORE_PARTNER_APPROVE",
+        )
+        val expiredByNotApproved = default.copy(
+            viewState = "EXPIRED_BY_NOT_APPROVED",
+        )
+        val approvedButBeforeStartDate = default.copy(
+            viewState = "APPROVED_BUT_BEFORE_START_DATE",
+        )
+        val inProgress = default.copy(
+            viewState = "IN_PROGRESS",
+        )
+        val complete = default.copy(
+            viewState = "COMPLETE",
+        )
+    }
+}
--- domain/src/main/java/model/challenge/response/UserResponseDomainModel.kt
+++ domain/src/main/java/model/challenge/response/UserResponseDomainModel.kt
@@ -10,4 +10,23 @@ data class UserResponseDomainModel(
     val partnerNo: Int,
     val socialId: String,
     val userNo: Int,
-)
+) {
+    companion object {
+        val user1 = UserResponseDomainModel(
+            deviceToken = "FCM TOKEN",
+            loginType = "Kakao",
+            nickname = "유저1",
+            partnerNo = 2,
+            socialId = "user1@test.com",
+            userNo = 1,
+        )
+        val user2 = UserResponseDomainModel(
+            deviceToken = "FCM TOKEN",
+            loginType = "Kakao",
+            nickname = "유저2",
+            partnerNo = 1,
+            socialId = "user2@test.com",
+            userNo = 2,
+        )
+    }
+}
