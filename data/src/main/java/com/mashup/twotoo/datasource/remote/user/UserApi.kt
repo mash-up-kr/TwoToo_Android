5a128466088c4633c7809b8f5e4758d6af69662c
ddyeon <cab05102@naver.com>
2023-07-13 21:21:58 +0900

    [feat] add userAPI

--- data/src/main/java/com/mashup/twotoo/datasource/remote/user/UserApi.kt
+++ data/src/main/java/com/mashup/twotoo/datasource/remote/user/UserApi.kt
@@ -1,33 +1,29 @@
 package com.mashup.twotoo.datasource.remote.user
 
-import com.mashup.twotoo.datasource.remote.user.response.PartnerInfoResponse
-import com.mashup.twotoo.datasource.remote.user.response.UserInfoModelResponse
+import com.mashup.twotoo.datasource.remote.user.request.UserAuthRequest
 import com.mashup.twotoo.datasource.remote.user.request.UserNickNameRequest
-import com.mashup.twotoo.datasource.remote.user.request.UserSignInRequest
-import com.mashup.twotoo.datasource.remote.user.request.UserSignUpRequest
+import com.mashup.twotoo.datasource.remote.user.response.PartnerInfoResponse
+import com.mashup.twotoo.datasource.remote.user.response.UserAuthResponse
+import com.mashup.twotoo.datasource.remote.user.response.UserInfoResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface UserApi {
-    @POST("/user/signup")
-    suspend fun signup(
-        @Body userSignUp: UserSignUpRequest
-    )
+    @POST("/user/authorize")
+    suspend fun userAuthorize(
+        @Body userAuthorize: UserAuthRequest
+    ): UserAuthResponse
 
-    @POST("/user/signin")
-    suspend fun signIn(
-        @Body userSignIn: UserSignInRequest
-    )
-
-    @POST("/user/nickname")
+    @PATCH("/user/nickname")
     suspend fun setUserNickName(
         @Body userNickName: UserNickNameRequest
-    )
+    ): UserInfoResponse
 
     @GET("/user/partner")
     suspend fun getUserPartnerInfo(): PartnerInfoResponse
 
     @GET("/user/me")
-    suspend fun getUserInfo(): UserInfoModelResponse
+    suspend fun getUserInfo(): UserInfoResponse
 }
