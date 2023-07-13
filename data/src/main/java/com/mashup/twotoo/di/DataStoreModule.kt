354962d241d087ad81936b7edff31b8dd5f9ed67
ddyeon <cab05102@naver.com>
2023-07-13 21:17:56 +0900

    [fix] datastoremodule error

--- data/src/main/java/com/mashup/twotoo/di/DataStoreModule.kt
+++ data/src/main/java/com/mashup/twotoo/di/DataStoreModule.kt
@@ -2,9 +2,10 @@ package com.mashup.twotoo.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.dataStoreFile
+import androidx.datastore.preferences.SharedPreferencesMigration
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -16,10 +17,12 @@ class DataStoreModule {
     @Singleton
     fun provideUserPreferenceDataStore(
         context: Context,
-    ): DataStore<Preferences> =
-        PreferenceDataStoreFactory.create {
-            context.dataStoreFile(USER_PREFERENCE)
-        }
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            migrations = listOf(SharedPreferencesMigration(context, USER_PREFERENCE)),
+            produceFile = { context.preferencesDataStoreFile(USER_PREFERENCE) },
+        )
+    }
 
     companion object {
         const val USER_PREFERENCE = "user_preferences.pb"
