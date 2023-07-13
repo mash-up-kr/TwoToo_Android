8affc13b1bb0aa58bed0d140f19a109a5b773602
Hyunkuk <boris0815@naver.com>
2023-07-13 18:26:32 +0900

    [feat] : add test fake repository

---
+++ presenter/src/test/java/com/mashup/twotoo/presenter/home/FakeViewRepository.kt
@@ -0,0 +1,41 @@
+package com.mashup.twotoo.presenter.home
+
+import model.challenge.response.HomeViewResponseDomainModel
+import repository.ViewRepository
+
+class FakeViewBeforeCreateRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.beforeCreate)
+    }
+}
+class FakeViewBeforeMyApproveRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.beforeMyApprove)
+    }
+}
+
+class FakeViewBeforePartnerApproveRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.beforePartnerApprove)
+    }
+}
+class FakeViewExpiredByNotApprovedRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.expiredByNotApproved)
+    }
+}
+class FakeViewApprovedButBeforeStartDateRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.approvedButBeforeStartDate)
+    }
+}
+class FakeViewInProgressRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.inProgress)
+    }
+}
+class FakeViewCompleteRepository : ViewRepository {
+    override suspend fun getViewHome(): Result<HomeViewResponseDomainModel> {
+        return Result.success(HomeViewResponseDomainModel.complete)
+    }
+}
