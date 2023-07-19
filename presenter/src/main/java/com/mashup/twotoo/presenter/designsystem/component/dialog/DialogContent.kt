d24f84bd015724b07dd093bdc8ab77ef3e47c35c
Hyunkuk <boris0815@naver.com>
2023-07-19 16:39:09 +0900

    [feat] : add new data class

--- presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/dialog/DialogContent.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/designsystem/component/dialog/DialogContent.kt
@@ -2,19 +2,21 @@ package com.mashup.twotoo.presenter.designsystem.component.dialog
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.mashup.twotoo.presenter.R
 
 data class DialogContent(
     @StringRes val title: Int,
     @StringRes val desc: Int,
-    @DrawableRes val image: Int?,
+    val dialogImage: DialogImage,
     val buttons: List<DialogButtonContent>,
 ) {
     companion object {
         val default = DialogContent(
             title = R.string.all_auth_title,
             desc = R.string.all_auth_desc,
-            image = R.drawable.img_home_dialog_all_verified,
+            dialogImage = DialogImage.bothAuth,
             buttons = emptyList(),
         )
         fun createHomeBothAuthDialogContent(
@@ -23,7 +25,7 @@ data class DialogContent(
         ) = DialogContent(
             title = R.string.all_auth_title,
             desc = R.string.all_auth_desc,
-            image = R.drawable.img_home_dialog_all_verified,
+            dialogImage = DialogImage.bothAuth,
             buttons = listOf(
                 DialogButtonContent(
                     text = R.string.cheer_up_cancel,
@@ -41,7 +43,7 @@ data class DialogContent(
         ) = DialogContent(
             title = R.string.congratulations,
             desc = R.string.congratulations_desc,
-            image = R.drawable.img_home_dialog_all_bloom,
+            dialogImage = DialogImage.bloomBoth,
             buttons = listOf(
                 DialogButtonContent(
                     text = R.string.button_confirm,
@@ -55,13 +57,46 @@ data class DialogContent(
         ) = DialogContent(
             title = R.string.thankyou,
             desc = R.string.thankyou_desc,
-            image = R.drawable.img_home_dialog_do_not_all_bloom,
+            dialogImage = DialogImage.doNotBloomBoth,
             buttons = listOf(
                 DialogButtonContent(
                     text = R.string.button_confirm,
                     action = onConfirm,
                 ),
             ),
+        )
+    }
+}
+
+data class DialogImage(
+    @DrawableRes val image: Int?,
+    val width: Dp,
+    val height: Dp,
+) {
+    companion object {
+
+        val bothAuth = DialogImage(
+            image = R.drawable.img_home_dialog_all_verified,
+            width = 123.dp,
+            height = 87.dp,
+        )
+
+        val bloomBoth = DialogImage(
+            image = R.drawable.img_home_dialog_all_bloom,
+            width = 196.dp,
+            height = 112.dp,
+        )
+
+        val doNotBloomBoth = DialogImage(
+            image = R.drawable.img_home_dialog_do_not_all_bloom,
+            width = 110.dp,
+            height = 103.dp,
+        )
+
+        val leaveChallenge = DialogImage(
+            image = R.drawable.crying_cloud,
+            width = 113.dp,
+            height = 96.dp,
         )
     }
 }
