baa762582b0ae3fce51b6ff4a4c9973d07f153d1
ddyeon <cab05102@naver.com>
2023-07-21 23:10:36 +0900

    [feat] add userauthresponse default value

--- domain/src/main/java/model/user/UserAuthResponseDomainModel.kt
+++ domain/src/main/java/model/user/UserAuthResponseDomainModel.kt
@@ -1,12 +1,13 @@
 package model.user
 
 data class UserAuthResponseDomainModel(
-    val socialId: String?,
-    val loginType: String,
-    val deviceToken: String,
-    val accessToken: String,
-    val state: String,
+    val socialId: String? = "",
+    val loginType: String = "",
+    val deviceToken: String = "",
+    val accessToken: String = "",
+    val state: String = "",
     val userNo: Int = 0,
     val nickname: String? = "",
     val partnerNo: Int? = 0
 )
+
