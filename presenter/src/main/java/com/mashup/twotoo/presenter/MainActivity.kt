5b8f43cea53c6c96931808084c09559982adf6b0
ddyeon <cab05102@naver.com>
2023-07-23 00:29:58 +0900

    [fix] remove mainactivity deeplink

--- presenter/src/main/java/com/mashup/twotoo/presenter/MainActivity.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.mashup.twotoo.presenter
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.google.firebase.dynamiclinks.ktx.dynamicLinks
-import com.google.firebase.ktx.Firebase
-import com.mashup.twotoo.presenter.constant.TAG
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
 import com.mashup.twotoo.presenter.twotoo.TwoTooApp
 
@@ -32,22 +26,6 @@ class MainActivity : ComponentActivity() {
                 }
                 TwoTooApp()
             }
-        }
-        checkInviteLink(intent)
-    }
-}
-
-private fun checkInviteLink(intent: Intent) {
-    Firebase.dynamicLinks.getDynamicLink(intent).addOnSuccessListener { linkData ->
-        var deepLink: Uri? = null
-        linkData?.let { data ->
-            deepLink = data.link
-        }
-        deepLink?.let { uri ->
-            val nickname = uri.getQueryParameter("nickname")
-            val userNo = uri.getQueryParameter("userNo")
-            Log.d(TAG, "checkInviteLink: $nickname")
-            Log.d(TAG, "checkInviteLink: $userNo")
         }
     }
 }
