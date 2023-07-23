50ef6192e1468669f87ca24d0f883056c0a81162
ddyeon <cab05102@naver.com>
2023-07-23 22:49:30 +0900

    [fix] 기존 toolbar 레이아웃 수정

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallenge.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallenge.kt
@@ -1,23 +1,18 @@
 package com.mashup.twotoo.presenter.createChallenge
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,69 +34,59 @@ fun CreateChallengeRoute(
 fun CreateChallenge(
     onFinishChallengeInfo: () -> Unit = {}
 ) {
-    var currentStep by remember { mutableStateOf(1) }
+    var currentStep by remember { mutableIntStateOf(1) }
     val challengeInfo = ChallengeInfoModel()
 
-    Scaffold(
-        topBar = {
-            TwoTooBackToolbar(onClickBackIcon = {
-                if (currentStep > 1) currentStep--
-            })
-        },
-    ) { padding ->
-        Box(
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        TwoTooBackToolbar(onClickBackIcon = {
+            if (currentStep > 1) currentStep--
+        })
+
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(padding)
-                .paint(
-                    painterResource(id = R.drawable.image_background),
-                    contentScale = ContentScale.FillBounds,
-                ),
+                .padding(top = 8.dp, start = 24.dp, end = 24.dp),
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 24.dp),
-            ) {
-                Text(
-                    text = stringResource(id = R.string.create_challenge_step, currentStep),
-                    textAlign = TextAlign.Left,
-                    style = TwoTooTheme.typography.headLineNormal28,
-                    color = TwoTooTheme.color.mainBrown,
-                )
-                Text(
-                    text = stringResource(id = R.string.create_challenge_desc_1),
-                    style = TwoTooTheme.typography.bodyNormal14,
-                    color = TwoTooTheme.color.gray600,
-                    modifier = Modifier.padding(top = 12.dp),
-                )
+            Text(
+                text = stringResource(id = R.string.create_challenge_step, currentStep),
+                textAlign = TextAlign.Left,
+                style = TwoTooTheme.typography.headLineNormal28,
+                color = TwoTooTheme.color.mainBrown,
+            )
+            Text(
+                text = stringResource(id = R.string.create_challenge_desc_1),
+                style = TwoTooTheme.typography.bodyNormal14,
+                color = TwoTooTheme.color.gray600,
+                modifier = Modifier.padding(top = 12.dp),
+            )
 
-                when (currentStep) {
-                    1 -> CreateChallengeOneStep()
-                    2 -> CreateChallengeTwoStep()
-                    3 -> CreateChallengeCard(
-                        "하루 운동 30분 이상 하기",
-                        "2023/05/01 ~ 5/22",
-                        "운동사진으로 인증하기\n실패하는 사람은 뷔페 쏘기",
-                    )
-                }
-
-                Spacer(Modifier.weight(1f))
-                TwoTooTextButton(
-                    text = stringResource(id = R.string.button_next),
-                    enabled = false,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(57.dp),
-                ) {
-                    if (currentStep == 3) {
-                        onFinishChallengeInfo()
-                    } else {
-                        currentStep++
-                    }
-                }
-                Spacer(modifier = Modifier.height(55.dp))
+            when (currentStep) {
+                1 -> CreateChallengeOneStep()
+                2 -> CreateChallengeTwoStep()
+                3 -> CreateChallengeCard(
+                    "하루 운동 30분 이상 하기",
+                    "2023/05/01 ~ 5/22",
+                    "운동사진으로 인증하기\n실패하는 사람은 뷔페 쏘기",
+                )
             }
+
+            Spacer(Modifier.weight(1f))
+            TwoTooTextButton(
+                text = stringResource(id = R.string.button_next),
+                enabled = false,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(57.dp),
+            ) {
+                if (currentStep == 3) {
+                    onFinishChallengeInfo()
+                } else {
+                    currentStep++
+                }
+            }
+            Spacer(modifier = Modifier.height(55.dp))
         }
     }
 }
