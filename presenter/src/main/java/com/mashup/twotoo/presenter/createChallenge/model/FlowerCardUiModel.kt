4df1c336be85f9511b3d7f8fe2b737d857551037
ddyeon <cab05102@naver.com>
2023-07-23 22:50:49 +0900

    [fix] 챌린지 생성 꽃선택 카드 인덱스 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/model/FlowerCardUiModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/model/FlowerCardUiModel.kt
@@ -8,6 +8,7 @@ data class FlowerCardUiModel(
     @StringRes val name: Int,
     @StringRes val desc: Int,
     @DrawableRes val selectFlowerImage: Int,
+    val index: Int
 ) {
     companion object {
         fun getFlowerCardModel(): List<FlowerCardUiModel> {
@@ -16,41 +17,49 @@ data class FlowerCardUiModel(
                     name = R.string.rose,
                     desc = R.string.rose_language,
                     selectFlowerImage = R.drawable.img_challenge_select_rose,
+                    0
                 ),
                 FlowerCardUiModel(
                     name = R.string.tulip,
                     desc = R.string.tulip_language,
                     selectFlowerImage = R.drawable.img_challenge_select_tulip,
+                    1
                 ),
                 FlowerCardUiModel(
                     name = R.string.fig,
                     desc = R.string.fig,
                     selectFlowerImage = R.drawable.img_challenge_select_fig,
+                    2
                 ),
                 FlowerCardUiModel(
                     name = R.string.cotton,
                     desc = R.string.cotton,
                     selectFlowerImage = R.drawable.img_challenge_select_cotton,
+                    3
                 ),
                 FlowerCardUiModel(
                     name = R.string.delphinium,
                     desc = R.string.delphinium,
                     selectFlowerImage = R.drawable.img_challenge_select_delphinium,
+                    4
                 ),
                 FlowerCardUiModel(
                     name = R.string.camellia,
                     desc = R.string.camellia,
                     selectFlowerImage = R.drawable.img_challenge_select_camellia,
+                    5
                 ),
                 FlowerCardUiModel(
                     name = R.string.chrysanthemum,
                     desc = R.string.chrysanthemum,
                     selectFlowerImage = R.drawable.img_challenge_select_chrysanthemum,
+                    6
                 ),
                 FlowerCardUiModel(
                     name = R.string.sunflower,
                     desc = R.string.sunflower,
                     selectFlowerImage = R.drawable.img_challenge_select_sunflower,
+                    7
                 ),
 
             )
