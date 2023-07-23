2bd6c97a32d8fd02e689400c80f14e6e6c35aba8
ddyeon <cab05102@naver.com>
2023-07-23 22:48:34 +0900

    [feat] 챌린지 생성 관련된 date formatter

--- presenter/src/main/java/com/mashup/twotoo/presenter/util/DateFormatter.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/util/DateFormatter.kt
@@ -14,4 +14,26 @@ object DateFormatter {
         val date = dateFormat.parse(date)
         return dateFormat.format(date)
     }
+
+    fun convertToLongDate(selectedDateMillis: Long?): String {
+        selectedDateMillis?.let { selectedDate ->
+            val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+            val date = Date(selectedDate)
+            return dateFormat.format(date)
+        }
+        return ""
+    }
+
+    fun getCurrentDate(): String {
+        return convertToLongDate(Calendar.getInstance().time.time)
+    }
+
+    fun getDaysAfter(selectDate: String, daysAfter: Int = 22): String {
+        val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+        val date = dateFormat.parse(selectDate)
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+        calendar.add(Calendar.DAY_OF_YEAR, daysAfter)
+        return convertToLongDate(calendar.time.time)
+    }
 }
