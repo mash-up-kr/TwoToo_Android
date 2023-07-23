355450175a64c4a992c91de279728f5840f3138a
ddyeon <cab05102@naver.com>
2023-07-23 23:11:44 +0900

    [feat] 챌린지 생성하기 API 연결

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallengeViewModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallengeViewModel.kt
@@ -2,17 +2,31 @@ package com.mashup.twotoo.presenter.createChallenge
 
 import androidx.lifecycle.ViewModel
 import com.mashup.twotoo.presenter.createChallenge.model.ChallengeInfoModel
+import model.challenge.request.CreateChallengeRequestDomainModel
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
+import usecase.challenge.CreateChallengeUseCase
+import javax.inject.Inject
 
-class CreateChallengeViewModel : ViewModel(), ContainerHost<CreateChallengeState, Unit> {
-    override val container = container<CreateChallengeState, Unit>(CreateChallengeState())
+class CreateChallengeViewModel@Inject constructor(
+    private val createChallengeUseCase: CreateChallengeUseCase
+) : ViewModel(), ContainerHost<CreateChallengeState, CreateChallengeSideEffect> {
+    override val container = container<CreateChallengeState, CreateChallengeSideEffect>(CreateChallengeState())
 
     fun setChallengeInfo(challengeInfoModel: ChallengeInfoModel) = intent {
         reduce {
             state.copy(challengeInfoModel = challengeInfoModel)
+        }
+    }
+
+    fun createChallenge() = intent {
+        createChallengeUseCase(CreateChallengeRequestDomainModel()).onSuccess {
+            postSideEffect(CreateChallengeSideEffect.NavigateToSuccessCreate)
+        }.onFailure {
+            postSideEffect(CreateChallengeSideEffect.ToastMessage(""))
         }
     }
 }
