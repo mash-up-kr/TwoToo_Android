8713515e35de7c5b728f3928f5f539004914e027
ddyeon <cab05102@naver.com>
2023-07-26 00:24:46 +0900

    [feat] date 변환 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/util/DateFormatter.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/util/DateFormatter.kt
@@ -1,6 +1,8 @@
 package com.mashup.twotoo.presenter.util
 
 import android.annotation.SuppressLint
+import android.util.Log
+import com.mashup.twotoo.presenter.constant.TAG
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -35,5 +37,28 @@ object DateFormatter {
         calendar.time = date
         calendar.add(Calendar.DAY_OF_YEAR, daysAfter)
         return convertToLongDate(calendar.time.time)
+    }
+
+    fun formatDateRange(startDate: String, endDate: String): String {
+        val inputFormat = SimpleDateFormat("yyyy/MM/dd", Locale.KOREA)
+        val outputFormat = SimpleDateFormat("yy/MM/dd", Locale.KOREA)
+
+        val startDateObj = inputFormat.parse(startDate)
+        val endDateObj = inputFormat.parse(endDate)
+
+        val formattedStartDate = startDateObj?.let { outputFormat.format(it) }
+        val formattedEndDate = endDateObj?.let { outputFormat.format(it) }
+
+        return "$formattedStartDate ~ $formattedEndDate"
+    }
+
+    fun convertToIsoTime(dateTime: String): String? {
+        val formatter = SimpleDateFormat("yyyy/MM/dd", Locale.KOREA)
+        val date = formatter.parse(dateTime)
+
+        val isoFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.KOREA)
+        isoFormatter.timeZone = TimeZone.getTimeZone("UTC")
+        Log.d(TAG, "convertToIsoTime:${isoFormatter.format(date)} ")
+        return date?.let { isoFormatter.format(it) }
     }
 }
