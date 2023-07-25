1d9f1e5e4668ecd7dd1ff65c7effa0c2e927d3cc
ddyeon <cab05102@naver.com>
2023-07-26 00:28:53 +0900

    [feat] 챌린지 생성 네비게이션

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/navigation/CreateChallengeNavigation.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/navigation/CreateChallengeNavigation.kt
@@ -1,17 +1,19 @@
 package com.mashup.twotoo.presenter.createChallenge.navigation
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.mashup.twotoo.presenter.createChallenge.CreateChallenge
+import com.mashup.twotoo.presenter.createChallenge.CreateChallengeRoute
 import com.mashup.twotoo.presenter.createChallenge.SuccessChallengeRequest
-import com.mashup.twotoo.presenter.createChallenge.di.CreateChallengeComponent
 import com.mashup.twotoo.presenter.createChallenge.di.CreateChallengeProvider
 import com.mashup.twotoo.presenter.createChallenge.selectflower.SelectFlowerCardRoute
 import com.mashup.twotoo.presenter.di.daggerViewModel
-import com.mashup.twotoo.presenter.mypage.di.UserComponentProvider
+import com.mashup.twotoo.presenter.home.navigation.navigateToHome
 import com.mashup.twotoo.presenter.navigation.NavigationRoute
 import com.mashup.twotoo.presenter.util.componentProvider
 
@@ -24,9 +26,10 @@ private fun NavController.navigateToSelectFlowerCard(navOptions: NavOptions? = n
 }
 
 private fun NavController.navigateToSuccessChallengeRequest(navOptions: NavOptions? = null) {
-    this.navigate(route = NavigationRoute.CreateChallengeGraph.route, navOptions = navOptions)
+    this.navigate(route = NavigationRoute.CreateChallengeGraph.SuccessChallengeRequest.route, navOptions = navOptions)
 }
 
+@SuppressLint("UnrememberedGetBackStackEntry")
 fun NavGraphBuilder.createChallengeGraph(
     navController: NavController
 ) {
@@ -39,17 +42,38 @@ fun NavGraphBuilder.createChallengeGraph(
             val createChallengeViewModel = daggerViewModel {
                 challengeComponent.getViewModel()
             }
-            CreateChallenge {
+            CreateChallengeRoute(
+                createChallengeViewModel = createChallengeViewModel,
+            ) {
                 navController.navigateToSelectFlowerCard()
             }
         }
         composable(route = NavigationRoute.CreateChallengeGraph.SelectFlowerCardScreen.route) {
-            SelectFlowerCardRoute {
-                navController.navigateToSuccessChallengeRequest()
+                navBackStackEntry ->
+            val parentRoute = NavigationRoute.CreateChallengeGraph.CreateChallengeScreen.route
+            val parentEntry = remember {
+                navController.getBackStackEntry(parentRoute)
             }
+            val viewModelStoreOwner: ViewModelStoreOwner = parentEntry
+
+            val challengeComponent = componentProvider<CreateChallengeProvider>().provideCreateChallengeComponent()
+            val createChallengeViewModel = daggerViewModel(viewModelStoreOwner = viewModelStoreOwner) {
+                challengeComponent.getViewModel()
+            }
+            SelectFlowerCardRoute(
+                createChallengeViewModel,
+                onClickBackButton = {
+                    navController.popBackStack()
+                },
+                onSuccessCreateChallenge = {
+                    navController.navigateToSuccessChallengeRequest()
+                },
+            )
         }
         composable(route = NavigationRoute.CreateChallengeGraph.SuccessChallengeRequest.route) {
-            SuccessChallengeRequest()
+            SuccessChallengeRequest {
+                navController.navigateToHome()
+            }
         }
     }
 }
