fc32bcd5e4191fde9a108e940bfdb421fd653c08
ddyeon <cab05102@naver.com>
2023-07-26 00:23:13 +0900

    [remove] remove challengestate

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallengeState.kt
+++
@@ -1,7 +0,0 @@
-package com.mashup.twotoo.presenter.createChallenge
-
-import com.mashup.twotoo.presenter.createChallenge.model.ChallengeInfoModel
-
-data class CreateChallengeState(
-    val challengeInfoModel: ChallengeInfoModel = ChallengeInfoModel()
-)
