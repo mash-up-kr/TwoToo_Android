dd008e194fc6825110e66e4b44197377c065425c
ddyeon <cab05102@naver.com>
2023-07-26 21:18:08 +0900

    [fix] card min width 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreatChallengeCard.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreatChallengeCard.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -91,7 +92,7 @@ fun ChallengeCardInfoTop(
     challengeDesc: String,
 ) {
     Column(
-        modifier = Modifier.wrapContentWidth()
+        modifier = Modifier.wrapContentWidth().defaultMinSize(minWidth = 242.dp)
             .padding(horizontal = 23.dp),
     ) {
         Text(
