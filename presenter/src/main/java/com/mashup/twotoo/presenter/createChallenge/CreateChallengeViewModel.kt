a9b5194b6d7e5c339d81aad10859a0fad53e4c58
ddyeon <cab05102@naver.com>
2023-07-28 18:07:53 +0900

    [feat] 챌린지 수락하기 추가

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallengeViewModel.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallengeViewModel.kt
@@ -5,20 +5,27 @@ import androidx.lifecycle.ViewModel
 import com.mashup.twotoo.presenter.constant.TAG
 import com.mashup.twotoo.presenter.createChallenge.model.ChallengeInfoModel
 import com.mashup.twotoo.presenter.createChallenge.model.toDomainModel
+import com.mashup.twotoo.presenter.home.model.HomeChallengeInfoModel
+import com.mashup.twotoo.presenter.util.DateFormatter
+import com.mashup.twotoo.presenter.util.DateFormatter.convertIsoTimeToString
+import model.challenge.request.ApproveChallengeRequestDomainModel
+import model.challenge.request.ChallengeNoRequestDomainModel
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
+import usecase.challenge.ApproveChallengeUseCase
 import usecase.challenge.CreateChallengeUseCase
 import javax.inject.Inject
 
 class CreateChallengeViewModel@Inject constructor(
+    private val approveChallengeUseCase: ApproveChallengeUseCase,
     private val createChallengeUseCase: CreateChallengeUseCase
 ) : ViewModel(), ContainerHost<ChallengeInfoModel, CreateChallengeSideEffect> {
     override val container = container<ChallengeInfoModel, CreateChallengeSideEffect>(ChallengeInfoModel())
 
-    fun setChallengeInfo(challengeInfo: ChallengeInfoModel, step: Int) = intent {
+    fun setCreateChallengeInfo(challengeInfo: ChallengeInfoModel, step: Int) = intent {
         reduce {
             when (step) {
                 1 -> { state.copy(
@@ -40,6 +47,19 @@ class CreateChallengeViewModel@Inject constructor(
         }
     }
 
+    fun setHomeChallengeInfo(homeChallengeInfoModel: HomeChallengeInfoModel) = intent {
+        reduce {
+            state.copy(
+                challengeName = homeChallengeInfoModel.name,
+                challengeInfo = homeChallengeInfoModel.description,
+                period = DateFormatter.formatDateRange(
+                    convertIsoTimeToString(homeChallengeInfoModel.startDate) ?: "",
+                    convertIsoTimeToString(homeChallengeInfoModel.endDate) ?: "",
+                ),
+            )
+        }
+    }
+
     fun createChallenge() = intent {
         createChallengeUseCase(state.toDomainModel()).onSuccess {
             postSideEffect(CreateChallengeSideEffect.NavigateToSuccessCreate)
@@ -47,6 +67,16 @@ class CreateChallengeViewModel@Inject constructor(
             Log.d(TAG, "fail:${it.message}")
             it.message
             postSideEffect(CreateChallengeSideEffect.ToastMessage(""))
+        }
+    }
+
+    fun approveChallenge(challengeNo: Int, selectFlower: String) = intent {
+        approveChallengeUseCase(
+            ChallengeNoRequestDomainModel(challengeNo),
+            ApproveChallengeRequestDomainModel(selectFlower),
+        ).onSuccess {
+            postSideEffect(CreateChallengeSideEffect.NavigateToHome)
+        }.onFailure {
         }
     }
 }
