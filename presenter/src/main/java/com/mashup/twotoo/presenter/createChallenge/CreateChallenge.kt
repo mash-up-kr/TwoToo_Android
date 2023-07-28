b069a01c2553a6197ed992a5ea8f24b24f31a669
ddyeon <cab05102@naver.com>
2023-07-28 18:08:28 +0900

    [fix] 챌린지 생성 상태에 따라 나누기

--- presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallenge.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/createChallenge/CreateChallenge.kt
@@ -17,15 +17,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mashup.twotoo.presenter.R
 import com.mashup.twotoo.presenter.constant.TAG
 import com.mashup.twotoo.presenter.createChallenge.model.ChallengeInfoModel
 import com.mashup.twotoo.presenter.designsystem.component.toolbar.TwoTooBackToolbar
 import com.mashup.twotoo.presenter.designsystem.theme.TwoTooTheme
+import com.mashup.twotoo.presenter.home.model.BeforeChallengeState
 import com.mashup.twotoo.presenter.util.DateFormatter
 import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
 fun CreateChallengeRoute(
+    homeState: String = BeforeChallengeState.EMPTY.name,
     step: Int = 1,
     createChallengeViewModel: CreateChallengeViewModel,
     onBackToHome: () -> Unit,
@@ -35,14 +38,15 @@ fun CreateChallengeRoute(
     var currentStep by remember { mutableIntStateOf(step) }
 
     CreateChallenge(
-        state,
-        currentStep,
+        homeState = homeState,
+        state = state,
+        currentStep = currentStep,
         updateOneStep = { challengeInfoModel ->
-            createChallengeViewModel.setChallengeInfo(challengeInfoModel, currentStep)
+            createChallengeViewModel.setCreateChallengeInfo(challengeInfoModel, currentStep)
             currentStep++
         },
         updateTwoStep = { challengeInfoModel ->
-            createChallengeViewModel.setChallengeInfo(challengeInfoModel, currentStep)
+            createChallengeViewModel.setCreateChallengeInfo(challengeInfoModel, currentStep)
             currentStep++
             Log.d(TAG, "CreateChallengeRoute: $state")
         },
@@ -50,8 +54,12 @@ fun CreateChallengeRoute(
             onFinishChallengeInfo()
         },
         onClickBackButton = {
-            if (currentStep > 1) {
-                currentStep--
+            if (homeState == BeforeChallengeState.EMPTY.name || homeState == BeforeChallengeState.TERMINATION.name) {
+                if (currentStep > 1) {
+                    currentStep--
+                } else {
+                    onBackToHome()
+                }
             } else {
                 onBackToHome()
             }
@@ -61,6 +69,7 @@ fun CreateChallengeRoute(
 
 @Composable
 fun CreateChallenge(
+    homeState: String = BeforeChallengeState.EMPTY.name,
     state: ChallengeInfoModel,
     currentStep: Int,
     updateOneStep: (ChallengeInfoModel) -> Unit,
@@ -69,32 +78,46 @@ fun CreateChallenge(
     onClickBackButton: () -> Unit
 ) {
     val context = LocalContext.current
+    val isNextButtonVisible = homeState == BeforeChallengeState.RESPONSE.name ||
+        homeState == BeforeChallengeState.EMPTY.name ||
+        homeState == BeforeChallengeState.TERMINATION.name
 
     Column(
         modifier = Modifier.fillMaxSize(),
     ) {
-        TwoTooBackToolbar(onClickBackIcon = {
-            onClickBackButton()
-        })
-
+        CreateChallengeToolbar(
+            homeState = homeState,
+            onClickBackButton = onClickBackButton,
+        )
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = 8.dp, start = 24.dp, end = 24.dp),
         ) {
-            Text(
-                text = stringResource(id = getCurrentStepString("title", currentStep, context), currentStep),
-                textAlign = TextAlign.Left,
-                style = TwoTooTheme.typography.headLineNormal28,
-                color = TwoTooTheme.color.mainBrown,
-            )
-            Text(
-                text = stringResource(id = getCurrentStepString("desc", currentStep, context)),
-                style = TwoTooTheme.typography.bodyNormal14,
-                color = TwoTooTheme.color.gray600,
-                modifier = Modifier.padding(top = 12.dp),
-            )
-
+            if (homeState == BeforeChallengeState.TERMINATION.name || homeState == BeforeChallengeState.EMPTY.name) {
+                Text(
+                    text = stringResource(id = R.string.create_challenge_step, currentStep),
+                    textAlign = TextAlign.Left,
+                    style = TwoTooTheme.typography.headLineNormal28,
+                    color = TwoTooTheme.color.mainBrown,
+                )
+            }
+            if (homeState == BeforeChallengeState.TERMINATION.name || homeState == BeforeChallengeState.EMPTY.name ||
+                homeState == BeforeChallengeState.RESPONSE.name
+            ) {
+                Text(
+                    text = stringResource(id = getCurrentStepString("title", currentStep, context), currentStep),
+                    textAlign = TextAlign.Left,
+                    style = TwoTooTheme.typography.headLineNormal28,
+                    color = TwoTooTheme.color.mainBrown,
+                )
+                Text(
+                    text = stringResource(id = getCurrentStepString("desc", currentStep, context)),
+                    style = TwoTooTheme.typography.bodyNormal14,
+                    color = TwoTooTheme.color.gray600,
+                    modifier = Modifier.padding(top = 12.dp),
+                )
+            }
             when (currentStep) {
                 1 -> CreateChallengeOneStep(state) { name, startDate, endDate ->
                     val period = DateFormatter.formatDateRange(startDate, endDate)
@@ -114,11 +137,27 @@ fun CreateChallenge(
                     challengeTitle = state.challengeName,
                     challengeDesc = state.challengeInfo,
                     challengeDate = state.period,
+                    isNextButtonVisible = isNextButtonVisible,
                     onClickNext = { onClickTheeStep() },
                 )
             }
         }
     }
+}
+
+@Composable
+fun CreateChallengeToolbar(homeState: String, onClickBackButton: () -> Unit) {
+    val toolbarTitle = when (homeState) {
+        BeforeChallengeState.WAIT.name, BeforeChallengeState.REQUEST.name -> stringResource(id = R.string.challenge_info)
+        BeforeChallengeState.RESPONSE.name, BeforeChallengeState.EMPTY.name, BeforeChallengeState.TERMINATION.name -> ""
+        else -> { "" }
+    }
+    TwoTooBackToolbar(
+        title = toolbarTitle,
+        onClickBackIcon = {
+            onClickBackButton()
+        },
+    )
 }
 
 fun getCurrentStepString(name: String, currentStep: Int, context: Context): Int {
