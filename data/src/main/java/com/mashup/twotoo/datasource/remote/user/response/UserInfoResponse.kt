1a21ef6274d935362d572236b80fd9c725421646
ddyeon <cab05102@naver.com>
2023-07-29 13:39:08 +0900

    [fix] userInfo partnerInfo nullable

--- data/src/main/java/com/mashup/twotoo/datasource/remote/user/response/UserInfoResponse.kt
+++ data/src/main/java/com/mashup/twotoo/datasource/remote/user/response/UserInfoResponse.kt
@@ -8,6 +8,6 @@ open class UserInfoResponse(
     @Json(name = "userNo") val userNo: Int = 0,
     @Json(name = "nickname") val nickname: String = "",
     @Json(name = "partnerNo") val partnerNo: Int? = 0,
-    @Json(name = "partnerNickname") val partnerNickName: String = "",
+    @Json(name = "partnerNickname") val partnerNickName: String? = "",
     @Json(name = "totalChallengeCount") val totalChallengeCount: Int = 0,
 )
--- domain/src/main/java/model/user/UserInfoDomainModel.kt
+++ domain/src/main/java/model/user/UserInfoDomainModel.kt
@@ -4,6 +4,6 @@ open class UserInfoDomainModel(
     val userNo: Int = 0,
     val nickname: String = "",
     val partnerNo: Int? = null,
-    val partnerNickname: String = "",
+    val partnerNickname: String? = "",
     val totalChallengeCount: Int = 0,
 )
