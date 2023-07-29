5e0e7e1d55145ed310eeabab705db1f4aa636f5e
ddyeon <cab05102@naver.com>
2023-07-30 00:29:00 +0900

    [fix] 홈화면 칭찬하기 버튼 리플 제거

--- presenter/src/main/java/com/mashup/twotoo/presenter/home/ongoing/components/HomeFlower.kt
+++ presenter/src/main/java/com/mashup/twotoo/presenter/home/ongoing/components/HomeFlower.kt
@@ -1,6 +1,7 @@
 package com.mashup.twotoo.presenter.home.ongoing.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -340,7 +342,10 @@ fun HomeCheerMe(
             HomeCheerFirstSpeech(
                 modifier = modifier.testTag(
                     stringResource(id = R.string.homeCheerChallengeMeBeforeCheerBubble),
-                ).padding(bottom = 32.dp).clickable {
+                ).padding(bottom = 32.dp).clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                ) {
                     onClickCheerButton()
                 },
                 cheerText = stringResource(id = R.string.homeCheerChallengeFirstText),
